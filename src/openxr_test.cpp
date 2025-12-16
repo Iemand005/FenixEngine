3b441c0a3ff98e390f8678b40eaa603755150c8c
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-12-16 17:55:57 +0100

    OpenXR Cube

--- src/openxr_test.cpp
+++ src/openxr_test.cpp
@@ -36,51 +36,50 @@ void outputError(XrResult result)
 
 float cubeVertices[] = {
     // positions          // colors
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
 
-    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
 
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
 
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
 
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
 
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f
-};
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f};
 
 // Shader source code
-const char* vertexShaderSource = R"(
+const char *vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
     layout (location = 1) in vec3 aColor;
@@ -97,7 +96,7 @@ const char* vertexShaderSource = R"(
     }
 )";
 
-const char* fragmentShaderSource = R"(
+const char *fragmentShaderSource = R"(
     #version 330 core
     in vec3 ourColor;
     out vec4 FragColor;
@@ -111,66 +110,69 @@ const char* fragmentShaderSource = R"(
 GLuint cubeVAO, cubeVBO, cubeShaderProgram;
 GLint modelLoc, viewLoc, projLoc;
 
-void initCube() {
-    // Create and compile shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    
-    // Create shader program
-    cubeShaderProgram = glCreateProgram();
-    glAttachShader(cubeShaderProgram, vertexShader);
-    glAttachShader(cubeShaderProgram, fragmentShader);
-    glLinkProgram(cubeShaderProgram);
-    
-    // Delete shaders (already linked)
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
-    // Get uniform locations
-    modelLoc = glGetUniformLocation(cubeShaderProgram, "model");
-    viewLoc = glGetUniformLocation(cubeShaderProgram, "view");
-    projLoc = glGetUniformLocation(cubeShaderProgram, "projection");
-    
-    // Set up vertex data and buffers
-    glGenVertexArrays(1, &cubeVAO);
-    glGenBuffers(1, &cubeVBO);
-    
-    glBindVertexArray(cubeVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-    
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    
-    std::cout << "Cube initialized\n";
+void initCube()
+{
+  // Create and compile shaders
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // Create shader program
+  cubeShaderProgram = glCreateProgram();
+  glAttachShader(cubeShaderProgram, vertexShader);
+  glAttachShader(cubeShaderProgram, fragmentShader);
+  glLinkProgram(cubeShaderProgram);
+
+  // Delete shaders (already linked)
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // Get uniform locations
+  modelLoc = glGetUniformLocation(cubeShaderProgram, "model");
+  viewLoc = glGetUniformLocation(cubeShaderProgram, "view");
+  projLoc = glGetUniformLocation(cubeShaderProgram, "projection");
+
+  // Set up vertex data and buffers
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &cubeVBO);
+
+  glBindVertexArray(cubeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+  // Position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  std::cout << "Cube initialized\n";
 }
 
-void drawCube(const glm::mat4& view, const glm::mat4& projection, const glm::mat4& model = glm::mat4(1.0f)) {
-    // Use shader program
-    glUseProgram(cubeShaderProgram);
-    
-    // Pass matrices to shader
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-    
-    // Draw cube
-    glBindVertexArray(cubeVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    glBindVertexArray(0);
+void drawCube(const glm::mat4 &view, const glm::mat4 &projection, const glm::mat4 &model = glm::mat4(1.0f))
+{
+  // Use shader program
+  glUseProgram(cubeShaderProgram);
+
+  // Pass matrices to shader
+  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+  glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+  // Draw cube
+  glBindVertexArray(cubeVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glBindVertexArray(0);
 }
 
 int main()
@@ -206,6 +208,12 @@ int main()
 
   HGLRC hglrc = wglGetCurrentContext();
 
+  if (!gladLoadGL())
+  {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
   XrGraphicsBindingOpenGLWin32KHR gfx{};
   gfx.type = XR_TYPE_GRAPHICS_BINDING_OPENGL_WIN32_KHR;
   gfx.hDC = GetDC(glfwGetWin32Window(window));
@@ -236,71 +244,14 @@ int main()
   beginInfo.primaryViewConfigurationType = XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO;
   outputError(xrBeginSession(session, &beginInfo));
 
-  // for (int i = 0; i < 100000000; i++)
-  // {
-  //   XrFrameWaitInfo waitInfo{XR_TYPE_FRAME_WAIT_INFO};
-  //   XrFrameState frameState{XR_TYPE_FRAME_STATE};
-  //   xrWaitFrame(session, &waitInfo, &frameState);
-
-  //   XrFrameBeginInfo frameBegin{XR_TYPE_FRAME_BEGIN_INFO};
-  //   xrBeginFrame(session, &frameBegin);
-
-  //   // Submit empty frame (black screen)
-  //   XrFrameEndInfo endInfo{XR_TYPE_FRAME_END_INFO};
-  //   endInfo.displayTime = frameState.predictedDisplayTime;
-  //   endInfo.layerCount = 0; // ZERO layers = black screen
-  //   xrEndFrame(session, &endInfo);
-  // }
-
   bool running = true;
+
+  initCube();
+
+  glEnable(GL_DEPTH_TEST);
 
   while (running)
   {
-    // XrEventDataBuffer event{XR_TYPE_EVENT_DATA_BUFFER};
-    // while (XR_SUCCEEDED(xrPollEvent(instance, &event)))
-    // {
-    //   if (event.type == XR_TYPE_EVENT_DATA_SESSION_STATE_CHANGED)
-    //   {
-    //     auto *stateChange = reinterpret_cast<XrEventDataSessionStateChanged *>(&event);
-    //     if (stateChange->state == XR_SESSION_STATE_STOPPING)
-    //     {
-    //       running = false;
-    //     }
-    //   }
-    //   event = {XR_TYPE_EVENT_DATA_BUFFER};
-    // }
-
-    // if (!running)
-    //   break;
-
-    // XrFrameWaitInfo waitInfo{XR_TYPE_FRAME_WAIT_INFO};
-    // XrFrameState frameState{XR_TYPE_FRAME_STATE};
-    // xrWaitFrame(session, &waitInfo, &frameState);
-
-    // XrFrameBeginInfo frameBegin{XR_TYPE_FRAME_BEGIN_INFO};
-    // xrBeginFrame(session, &frameBegin);
-
-    // XrSwapchainImageAcquireInfo acquireInfo{XR_TYPE_SWAPCHAIN_IMAGE_ACQUIRE_INFO};
-    // uint32_t swapchainImageIndex;
-    // xrAcquireSwapchainImage(swapchain, &acquireInfo, &swapchainImageIndex);
-
-    // // Wait until image is ready
-    // XrSwapchainImageWaitInfo waitImageInfo{XR_TYPE_SWAPCHAIN_IMAGE_WAIT_INFO};
-    // waitImageInfo.timeout = XR_INFINITE_DURATION;
-    // xrWaitSwapchainImage(swapchain, &waitImageInfo);
-
-    // // 5. Locate views
-    // XrViewState viewState{XR_TYPE_VIEW_STATE};
-    // XrViewLocateInfo viewLocateInfo{XR_TYPE_VIEW_LOCATE_INFO};
-    // viewLocateInfo.viewConfigurationType = XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO;
-    // viewLocateInfo.displayTime = frameState.predictedDisplayTime;
-    // viewLocateInfo.space = appSpace;
-
-    // uint32_t viewCount;
-    // xrLocateViews(session, &viewLocateInfo, &viewState, views.size(), &viewCount, views.data());
-
-    // // 6. Render to each eye
-    // std::vector<XrCompositionLayerProjectionView> projectionViews(viewCount);
 
     XrFrameWaitInfo waitInfo{XR_TYPE_FRAME_WAIT_INFO};
     XrFrameState frameState{XR_TYPE_FRAME_STATE};
@@ -310,30 +261,27 @@ int main()
     xrBeginFrame(session, &frameBegin);
 
     // SIMPLE: Render cube to default framebuffer (for testing)
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Simple rotating cube
+    // Calculate matrices
     static float angle = 0.0f;
     angle += 0.01f;
 
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.5f, 1.0f, 0.0f));
     glm::mat4 view = glm::lookAt(
-        glm::vec3(0.0f, 0.0f, 3.0f), // Camera position
+        glm::vec3(2.0f, 2.0f, 2.0f), // Camera position
         glm::vec3(0.0f, 0.0f, 0.0f), // Look at origin
         glm::vec3(0.0f, 1.0f, 0.0f)  // Up vector
     );
-
     glm::mat4 projection = glm::perspective(
         glm::radians(45.0f), // FOV
         16.0f / 9.0f,        // Aspect ratio
         0.1f, 100.0f         // Near/far
     );
 
-    glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.5f, 1.0f, 0.0f));
-    glm::mat4 mvp = projection * view * model;
-
-    // Render cube (using your cube rendering code)
-    renderCube(mvp);
+    // Draw the cube
+    drawCube(view, projection, model);
 
     // End frame with NO layers (still shows something in desktop window)
     XrFrameEndInfo endInfo{XR_TYPE_FRAME_END_INFO};
