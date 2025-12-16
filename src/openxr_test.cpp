ba7a96386ccdf84c1441170a5e058ba89e1df5d4
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-12-16 06:55:05 +0100

    OpenXR working start

--- src/openxr_test.cpp
+++ src/openxr_test.cpp
@@ -1,6 +1,7 @@
 #define XR_USE_PLATFORM_WIN32
 #define XR_USE_GRAPHICS_API_OPENGL
 #define GLFW_EXPOSE_NATIVE_WIN32
+// #define XR_EXTENSION_PROTOTYPES
 // #define XR_KHR_opengl_enable
 
 #include <Windows.h>
@@ -20,6 +21,10 @@ int main()
 {
   XrInstance instance;
   XrInstanceCreateInfo createInfo{XR_TYPE_INSTANCE_CREATE_INFO};
+
+  const char* enabledExtensions[] = { XR_KHR_OPENGL_ENABLE_EXTENSION_NAME };
+    createInfo.enabledExtensionCount = 1;
+    createInfo.enabledExtensionNames = enabledExtensions;
 
   createInfo.applicationInfo.apiVersion = XR_API_VERSION_1_0;
   createInfo.applicationInfo.applicationVersion = 1;
@@ -82,67 +87,112 @@ int main()
 //   gfxBinding.hGLRC = glfwWGL();
 // glfwGetWin32Window
 
-     HWND hwnd = CreateWindowA(
-        "EDIT", 
-        "OpenXR Binding", 
-        WS_OVERLAPPEDWINDOW,
-        0, 0, 1, 1, 
-        NULL, NULL, 
-        GetModuleHandle(NULL), 
-        NULL
-    );
-    ShowWindow(hwnd, SW_HIDE);
-    HDC hdc = GetDC(hwnd);
+//      HWND hwnd = CreateWindowA(
+//         "EDIT", 
+//         "OpenXR Binding", 
+//         WS_OVERLAPPEDWINDOW,
+//         0, 0, 1, 1, 
+//         NULL, NULL, 
+//         GetModuleHandle(NULL), 
+//         NULL
+//     );
+//     ShowWindow(hwnd, SW_HIDE);
+//     HDC hdc = GetDC(hwnd);
     
-    // Setup pixel format
-    // PIXELFORMATDESCRIPTOR pfd = { sizeof(PIXELFORMATDESCRIPTOR), 1, ... };
-    // int format = ChoosePixelFormat(hdc, &pfd);
-    // SetPixelFormat(hdc, format, &pfd);
+//     // Setup pixel format
+//     // PIXELFORMATDESCRIPTOR pfd = { sizeof(PIXELFORMATDESCRIPTOR), 1, ... };
+//     // int format = ChoosePixelFormat(hdc, &pfd);
+//     // SetPixelFormat(hdc, format, &pfd);
 
-    PIXELFORMATDESCRIPTOR pfd = {
-    sizeof(PIXELFORMATDESCRIPTOR),
-    1,
-    PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
-    PFD_TYPE_RGBA,
-    32, // Color depth
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    24, // Depth buffer
-    8,  // Stencil buffer
+//     PIXELFORMATDESCRIPTOR pfd = {
+//     sizeof(PIXELFORMATDESCRIPTOR),
+//     1,
+//     PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+//     PFD_TYPE_RGBA,
+//     32, // Color depth
+//     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     24, // Depth buffer
+//     8,  // Stencil buffer
     
-};
+// };
 
-int pixelFormat = ChoosePixelFormat(hdc, &pfd);
-if (pixelFormat == 0) {
-    // Handle error
-}
+// int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+// if (pixelFormat == 0) {
+//     // Handle error
+// }
 
-if (!SetPixelFormat(hdc, pixelFormat, &pfd)) {
-    // Handle error
-}
+// if (!SetPixelFormat(hdc, pixelFormat, &pfd)) {
+//     // Handle error
+// }
     
-    // Create REAL WGL context
-    HGLRC hglrc = wglCreateContext(hdc);
-    DWORD error = GetLastError();
-    wglMakeCurrent(hdc, hglrc);
+//     // Create REAL WGL context
+//     HGLRC hglrc = wglCreateContext(hdc);
+//     DWORD error = GetLastError();
+//     wglMakeCurrent(hdc, hglrc);
     
-    // Load GLAD
-    gladLoadGL();
+//     // Load GLAD
+//     gladLoadGL();
+
+glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    auto window = glfwCreateWindow(800, 600, "FoxEngine", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    HGLRC hglrc = wglGetCurrentContext();
     
     // Now use for OpenXR
     XrGraphicsBindingOpenGLWin32KHR gfx{};
     gfx.type = XR_TYPE_GRAPHICS_BINDING_OPENGL_WIN32_KHR;
-    gfx.hDC = hdc;
+    gfx.hDC = GetDC(glfwGetWin32Window(window));
     gfx.hGLRC = hglrc;
 
   XrSessionCreateInfo sci{XR_TYPE_SESSION_CREATE_INFO};
   sci.systemId = systemId;
   sci.next = &gfx;
 
-  // 3. Create Session (NULL graphics binding - will fail on some runtimes)
   XrSession session;
   XrSessionCreateInfo sessionInfo{XR_TYPE_SESSION_CREATE_INFO};
   sessionInfo.systemId = systemId;
+  sessionInfo.next = &gfx;
 
+  // XrGraphicsRequirementsOpenGLKHR vulkanReqs = { XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR };
+  XrGraphicsRequirementsOpenGLKHR glReqs = { XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR };
+// XrResult result = PFN_xrGetOpenGLGraphicsRequirementsKHR(instance, systemId, &vulkanReqs);
+// if (xrGetOpenGLGraphicsRequirementsKHR(instance, systemId, &glReqs) != XR_SUCCESS) {
+//         // Handle error: Runtime may not support your OpenGL version
+//         printf("OpenGL version not supported. Min: %d.%d\n",
+//                XR_VERSION_MAJOR(glReqs.minApiVersionSupported),
+//                XR_VERSION_MINOR(glReqs.minApiVersionSupported));
+//         return -1;
+//     }
+PFN_xrGetOpenGLGraphicsRequirementsKHR pfnGetOpenGLGraphicsRequirementsKHR = nullptr;
+    
+    result = xrGetInstanceProcAddr(instance,
+                                             "xrGetOpenGLGraphicsRequirementsKHR",
+                                             (PFN_xrVoidFunction*)(&pfnGetOpenGLGraphicsRequirementsKHR));
+    
+    if (result != XR_SUCCESS || pfnGetOpenGLGraphicsRequirementsKHR == nullptr) {
+        std::cerr << "Failed to get xrGetOpenGLGraphicsRequirementsKHR function pointer. Result: " << result << std::endl;
+        xrDestroyInstance(instance);
+        return -1;
+    }
+    std::cout << "Successfully retrieved function pointer." << std::endl;
+
+    // 5. NOW call the function using your pointer
+    
+    if (pfnGetOpenGLGraphicsRequirementsKHR(instance, systemId, &glReqs) != XR_SUCCESS) {
+        std::cerr << "Failed to get OpenGL graphics requirements." << std::endl;
+        xrDestroyInstance(instance);
+        return -1;
+    }
+    std::cout << "Successfully got OpenGL requirements." << std::endl;
+
+
+if (XR_FAILED(result)) { /* Handle error */ }
+  
   result = xrCreateSession(instance, &sessionInfo, &session);
   if (result != XR_SUCCESS)
   {
@@ -159,7 +209,7 @@ if (!SetPixelFormat(hdc, pixelFormat, &pfd)) {
   xrBeginSession(session, &beginInfo);
 
   // 5. Run for 10 seconds of black screen
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < 100000000; i++)
   {
     XrFrameWaitInfo waitInfo{XR_TYPE_FRAME_WAIT_INFO};
     XrFrameState frameState{XR_TYPE_FRAME_STATE};
