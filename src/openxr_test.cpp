def37112c13a5d409412891618c5a6ebfdd26446
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-12-16 21:01:04 +0100

    Adddepth for VR eyes

--- src/openxr_test.cpp
+++ src/openxr_test.cpp
@@ -29,151 +29,9 @@ void outputError(XrResult result)
     return;
   std::cerr << "Failed to create instance: " << result << "\n";
 
-  // Try to get error info
   char buf[XR_MAX_RESULT_STRING_SIZE];
   if (xrResultToString(nullptr, result, buf) == XR_SUCCESS)
     std::cerr << "Error: " << buf << "\n";
-}
-
-float cubeVertices[] = {
-    // positions          // colors
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-
-    -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-    0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
-
-    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-
-    0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
-    0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
-    0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
-    0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
-    0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
-    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
-
-    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
-    0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
-    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
-    0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
-    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
-    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f};
-
-// Shader source code
-const char *vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec3 aColor;
-    
-    out vec3 ourColor;
-    
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 projection;
-    
-    void main() {
-        gl_Position = projection * view * model * vec4(aPos, 1.0);
-        ourColor = aColor;
-    }
-)";
-
-const char *fragmentShaderSource = R"(
-    #version 330 core
-    in vec3 ourColor;
-    out vec4 FragColor;
-    
-    void main() {
-        FragColor = vec4(ourColor, 1.0);
-    }
-)";
-
-// Global OpenGL objects
-GLuint cubeVAO, cubeVBO, cubeShaderProgram;
-GLint modelLoc, viewLoc, projLoc;
-
-void initCube()
-{
-  // Create and compile shaders
-
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  // Create shader program
-  cubeShaderProgram = glCreateProgram();
-  glAttachShader(cubeShaderProgram, vertexShader);
-  glAttachShader(cubeShaderProgram, fragmentShader);
-  glLinkProgram(cubeShaderProgram);
-
-  // Delete shaders (already linked)
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
-
-  // Get uniform locations
-  modelLoc = glGetUniformLocation(cubeShaderProgram, "model");
-  viewLoc = glGetUniformLocation(cubeShaderProgram, "view");
-  projLoc = glGetUniformLocation(cubeShaderProgram, "projection");
-
-  // Set up vertex data and buffers
-  glGenVertexArrays(1, &cubeVAO);
-  glGenBuffers(1, &cubeVBO);
-
-  glBindVertexArray(cubeVAO);
-  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-
-  // Position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // Color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
-
-  std::cout << "Cube initialized\n";
-}
-
-void drawCube(const glm::mat4 &view, const glm::mat4 &projection, const glm::mat4 &model = glm::mat4(1.0f))
-{
-  // Use shader program
-  glUseProgram(cubeShaderProgram);
-
-  // Pass matrices to shader
-  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-  glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-  // Draw cube
-  glBindVertexArray(cubeVAO);
-  glDrawArrays(GL_TRIANGLES, 0, 36);
-  glBindVertexArray(0);
 }
 
 XrInstance instance;
@@ -183,14 +41,13 @@ XrSpace appSpace = XR_NULL_HANDLE;
 
 XrSwapchain swapchain;
 std::vector<XrSwapchainImageOpenGLKHR> swapchainImages;
+std::vector<GLuint> depthTextures;
 std::vector<GLuint> framebuffers;
 uint32_t viewCount = 2; // Stereo
 int32_t swapchainWidth, swapchainHeight;
 
-// Initialize swapchain (call after session creation)
 void initSwapchain(XrSession session)
 {
-  // Get recommended view configuration
   uint32_t configCount;
   xrEnumerateViewConfigurationViews(instance, systemId,
                                     XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO, 0, &configCount, nullptr);
@@ -203,7 +60,6 @@ void initSwapchain(XrSession session)
   swapchainWidth = configViews[0].recommendedImageRectWidth;
   swapchainHeight = configViews[0].recommendedImageRectHeight;
 
-  // Create swapchain
   XrSwapchainCreateInfo swapchainInfo{XR_TYPE_SWAPCHAIN_CREATE_INFO};
   swapchainInfo.arraySize = viewCount; // Stereo = 2 layers
   swapchainInfo.format = GL_SRGB8_ALPHA8;
@@ -216,22 +72,25 @@ void initSwapchain(XrSession session)
 
   xrCreateSwapchain(session, &swapchainInfo, &swapchain);
 
-  // Get swapchain images
   uint32_t imageCount;
   xrEnumerateSwapchainImages(swapchain, 0, &imageCount, nullptr);
   swapchainImages.resize(imageCount, {XR_TYPE_SWAPCHAIN_IMAGE_OPENGL_KHR});
-  xrEnumerateSwapchainImages(swapchain, imageCount, &imageCount,
-                             (XrSwapchainImageBaseHeader *)swapchainImages.data());
+  xrEnumerateSwapchainImages(swapchain, imageCount, &imageCount, (XrSwapchainImageBaseHeader *)swapchainImages.data());
+                  
+                             depthTextures = std::vector<GLuint>(imageCount);
 
-  // Create framebuffers for each swapchain image
+  glGenTextures(imageCount, depthTextures.data());
+
   framebuffers.resize(imageCount);
   glGenFramebuffers(imageCount, framebuffers.data());
 
   for (int i = 0; i < imageCount; i++)
   {
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffers[i]);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                           GL_TEXTURE_2D_ARRAY, swapchainImages[i].image, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_ARRAY, swapchainImages[i].image, 0);
+
+    glBindTexture(GL_TEXTURE_2D_ARRAY, depthTextures[i]);
+    glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_DEPTH_COMPONENT32F, swapchainWidth, swapchainHeight, 2, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
   }
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
@@ -344,7 +203,6 @@ int main()
 
   initSwapchain(session);
 
-  initCube();
 
   auto scene = std::make_unique<fe::Scene>();
   auto shader = std::make_unique<fe::ShaderProgram>("VertexShader.glsl", "FragmentShader.glsl");
@@ -386,13 +244,11 @@ int main()
     viewLocateInfo.space = appSpace;
 
     uint32_t viewCount = 0;
-    outputError(xrEnumerateViewConfigurationViews(instance, systemId,
-                                                  XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO, 0, &viewCount, nullptr));
+    outputError(xrEnumerateViewConfigurationViews(instance, systemId, XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO, 0, &viewCount, nullptr));
 
-    std::vector<XrView> views(viewCount, {XR_TYPE_VIEW}); // Initialize with correct type
+    std::vector<XrView> views(viewCount, {XR_TYPE_VIEW});
     std::vector<XrViewConfigurationView> viewConfigs(viewCount, {XR_TYPE_VIEW_CONFIGURATION_VIEW});
-    outputError(xrEnumerateViewConfigurationViews(instance, systemId,
-                                                  XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO, viewCount, &viewCount, viewConfigs.data()));
+    outputError(xrEnumerateViewConfigurationViews(instance, systemId, XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO, viewCount, &viewCount, viewConfigs.data()));
 
     outputError(xrLocateViews(session, &viewLocateInfo, &viewState, viewCount, &viewCount, views.data()));
 
@@ -402,10 +258,19 @@ int main()
     static float angle = 0.0f;
     static fe::Camera camera;
 
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+
+    // glDepthRangef(0.0f, 1.0f);
+
     for (uint32_t eye = 0; eye < viewCount; eye++)
     {
-      glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                                swapchainImages[swapchainImageIndex].image, 0, eye);
+      glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, swapchainImages[swapchainImageIndex].image, 0, eye);
+
+      glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTextures[swapchainImageIndex], 0, eye);
 
       glViewport(0, 0, swapchainWidth, swapchainHeight);
 
@@ -415,72 +280,30 @@ int main()
       XrPosef pose = views[eye].pose;
       XrFovf fov = views[eye].fov;
 
-      glm::mat4 view = glm::inverse(
-          glm::translate(glm::mat4(1.0f),
-                         glm::vec3(pose.position.x, pose.position.y, pose.position.z)) *
-          glm::mat4_cast(glm::quat(pose.orientation.w,
-                                   pose.orientation.x, pose.orientation.y, pose.orientation.z)));
+      glm::vec3 position(pose.position.x, pose.position.y, pose.position.z);
+      glm::quat orientation(pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z);
 
-      float tanLeft = tanf(fov.angleLeft);
-      float tanRight = tanf(fov.angleRight);
-      float tanDown = tanf(fov.angleDown);
-      float tanUp = tanf(fov.angleUp);
+      auto front = orientation * glm::vec3(0.0f, 0.0f, -1.0f);
+      auto up = orientation * glm::vec3(0.0f, 1.0f, 0.0f);
 
-      glm::mat4 projection = glm::mat4(0.0f);
-      projection[0][0] = 2.0f / (tanRight - tanLeft);
-      projection[1][1] = 2.0f / (tanUp - tanDown);
-      projection[2][0] = (tanRight + tanLeft) / (tanRight - tanLeft);
-      projection[2][1] = (tanUp + tanDown) / (tanUp - tanDown);
-      projection[2][2] = -0.1f - 100.0f / (100.0f - 0.1f);
-      projection[2][3] = -1.0f;
-      projection[3][2] = -(0.1f * 100.0f) / (100.0f - 0.1f);
+      float nearDist = 0.10f;
+      float farDist = 100.0f;
 
-      glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle,
-                                    glm::vec3(0.5f, 1.0f, 0.0f));
+      float left = tan(fov.angleLeft) * nearDist;
+      float right = tan(fov.angleRight) * nearDist;
+      float bottom = tan(fov.angleDown) * nearDist;
+      float top = tan(fov.angleUp) * nearDist;
 
-      drawCube(view, projection, model);
 
-      glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+      camera = fe::Camera(glm::vec3(pose.position.x, pose.position.y, pose.position.z), front, up, 45.0f, 1.0f, nearDist, farDist);
 
-auto position = glm::vec3(pose.position.x, pose.position.y, pose.position.z);
-    
-    // Orientation quaternion
-    glm::quat orientation(pose.orientation.w, 
-                          pose.orientation.x, 
-                          pose.orientation.y, 
-                          pose.orientation.z);
-    
-    // Compute forward vector (OpenXR uses -Z as forward)
-    auto front = orientation * glm::vec3(0.0f, 0.0f, -1.0f);
-    // Compute up vector
-    auto up = orientation * glm::vec3(0.0f, 1.0f, 0.0f);
+      camera.projectionMatrix = glm::frustum(left, right, bottom, top, nearDist, farDist);
 
-    float nearDist = 0.10f;
-    float farDist = 100.0f;
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    float left   = tan(fov.angleLeft)   * nearDist;
-    float right  = tan(fov.angleRight)  * nearDist;
-    float bottom = tan(fov.angleDown)   * nearDist;
-    float top    = tan(fov.angleUp)     * nearDist;
-    
-    // Create asymmetric projection matrix
-    // auto projectionMatrix = glm::frustum(left, right, bottom, top, nearDist, farDist);
+      scene->render(*shader, camera);
 
-    camera = fe::Camera(glm::vec3(pose.position.x, pose.position.y, pose.position.z), front, up, 45.0f, 1.0f,  nearDist, farDist);
-
-    camera.projectionMatrix = glm::frustum(left, right, bottom, top, nearDist, farDist);
-
-      scene->prepareRender(*shader, camera);
-
-      for (auto &model : scene->getModels())
-      {
-        model->render(*shader);
-      }
-
-      // Store projection view for layer submission
       projectionViews[eye] = {XR_TYPE_COMPOSITION_LAYER_PROJECTION_VIEW};
       projectionViews[eye].pose = views[eye].pose;
       projectionViews[eye].fov = views[eye].fov;
@@ -500,12 +323,11 @@ auto position = glm::vec3(pose.position.x, pose.position.y, pose.position.z);
     layer.viewCount = viewCount;
     layer.views = projectionViews.data();
 
-    const XrCompositionLayerBaseHeader *layers[] = {
-        (XrCompositionLayerBaseHeader *)&layer};
+    const XrCompositionLayerBaseHeader *layers[] = {(XrCompositionLayerBaseHeader *)&layer};
 
     XrFrameEndInfo endInfo{XR_TYPE_FRAME_END_INFO};
     endInfo.displayTime = frameState.predictedDisplayTime;
-    endInfo.layerCount = 1; // MUST be > 0 to see in headset!
+    endInfo.layerCount = 1;
     endInfo.layers = layers;
     endInfo.environmentBlendMode = XR_ENVIRONMENT_BLEND_MODE_OPAQUE;
     outputError(xrEndFrame(session, &endInfo));
@@ -515,31 +337,17 @@ auto position = glm::vec3(pose.position.x, pose.position.y, pose.position.z);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw cube to window (optional)
-    glm::mat4 windowView = glm::lookAt(
-        glm::vec3(2.0f, 2.0f, 2.0f),
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 windowProj = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 100.0f);
-    angle += 0.01f;
-    glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle,
-                                  glm::vec3(0.5f, 1.0f, 0.0f));
-
-    drawCube(windowView, windowProj);
     scene->render(*(shader), (camera));
 
     glfwSwapBuffers(window);
     glfwPollEvents();
 
     if (glfwWindowShouldClose(window))
-    {
       running = false;
-    }
   }
 
   std::cout << "Done!\n";
 
-  // Cleanup
   xrDestroySession(session);
   xrDestroyInstance(instance);
 
