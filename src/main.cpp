297d32645e26b06b3d5fe59256df099ff2c4faa8
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-12-25 22:04:02 +0100

    rawr

--- src/main.cpp
+++ src/main.cpp
@@ -544,14 +544,13 @@ class Game {
 };
 
 int main() {
-  Game game(800, 600);
-unsigned long long int hey = 0;
-// unsigned int32_t meow = 10;
-// memcpy()
-char *test = "maow";
-test[0]++;
-printf("%s", test);
+  
+  char testBuffer[5] = "maow";
+  testBuffer[0]++;
+  printf("%s", testBuffer);
 
+  Game game(800, 600);
+  
   glm::vec3 playerHeight = glm::vec3(0.0f, 6.5f, 0.0f);
 
   while (!game.shouldClose()) {
