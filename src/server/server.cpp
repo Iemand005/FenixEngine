4c71e7cbf5d9137729118e214adb5f5f7d6a1220
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-01-02 11:06:45 +0100

    garabage cleanup and fix obj filez

---
+++ src/server/server.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <system_error>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#define CLOSE_SOCKET closesocket
+#define SOCKET_ERRNO WSAGetLastError()
+using socket_t = SOCKET;
+
+#include "engine/networking/networking.hpp"
+
+class Server
+{
+public:
+
+  Networker server;
+
+  Server() {
+    server = Networker(2130);
+  }
+
+  void start()
+  {
+    server.receiveHandler = [this](const char *data, size_t size, PacketType type, ClientData sender) {
+      // if (type != PacketType::Hello)
+      //   server.broadcast(data, size);
+    };
+    server.setMessageReceiveHandler([](std::string message, ClientData sender){
+      std::cout << "Message reached server: " << message << " with client ID: " << (int)sender.id << " and username: " << sender.username << std::endl;
+    });
+
+    // server.socket.createSocketIfNotExist();
+    server.startServer();
+  }
+
+  
+};
+
+int main()
+{
+  Server server;
+  server.start();
+
+  return 0;
+}
