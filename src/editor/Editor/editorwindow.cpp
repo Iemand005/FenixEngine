796a5079362fde450caa660adc4401f4ead9eec3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-05 07:21:20 +0100

    Add QT editor base project

---
+++ src/editor/Editor/editorwindow.cpp
@@ -0,0 +1,7 @@
+#include "editorwindow.h"
+
+#include "ui_editorwindow.h"
+
+EditorWindow::EditorWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::EditorWindow) { ui->setupUi(this); }
+
+EditorWindow::~EditorWindow() { delete ui; }
---
+++ src/editor/Editor/main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+#include "editorwindow.h"
+
+int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
+
+  QTranslator translator;
+  const QStringList uiLanguages = QLocale::system().uiLanguages();
+  for (const QString& locale : uiLanguages) {
+    const QString baseName = "Editor_" + QLocale(locale).name();
+    if (translator.load(":/i18n/" + baseName)) {
+      a.installTranslator(&translator);
+      break;
+    }
+  }
+  EditorWindow w;
+  w.show();
+  return a.exec();
+}
