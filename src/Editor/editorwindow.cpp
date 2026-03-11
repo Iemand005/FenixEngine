0414048e5c976d13d81eb7acb590a2d3d84ce59c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-11 07:51:47 +0100

    signals n slots

--- src/Editor/editorwindow.cpp
+++ src/Editor/editorwindow.cpp
@@ -2,10 +2,24 @@
 
 #include "ui_editorwindow.h"
 
-EditorWindow::EditorWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::EditorWindow) { ui->setupUi(this); }
+#include <QPushButton>
+
+EditorWindow::EditorWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::EditorWindow) {
+  ui->setupUi(this);
+
+  connect(ui->shaderButton, &QPushButton::clicked, []() {
+
+  });
+}
 
 EditorWindow::~EditorWindow() { delete ui; }
 
 void EditorWindow::compileShaders() {
   // engineWidget->getGame()->shader->LoadShaders("", "");
+  // this->engineWidget()
+
+  auto game = this->ui->engineWidget->getGame();
+  // this->ui->shaderButton()
+
+  ui->engineWidget->getGame()->shader->LoadShaderTexts("", "");
 }
