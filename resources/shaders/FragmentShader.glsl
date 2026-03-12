c132e52d97d834e19ec74d35316c904c61be5e10
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-12 05:40:08 +0100

    Update fragment shader

--- resources/shaders/FragmentShader.glsl
+++ resources/shaders/FragmentShader.glsl
@@ -5,13 +5,18 @@ in vec3 normal;
 in vec2 TexCoord;
 
 uniform sampler2D ourTexture;
-  
-// uniform vec4 ourColor; // we set this variable in the OpenGL code.
 
 void main()
 {
-    // FragColor = vec4(vec3(1.0) * normal.x * 1 + 0.5, 1.0);
-    // FragColor = texture(ourTexture, TexCoord) * vec4(vec3(1.0) * normal.x * 1 + 0.5, 1.0);
-    // FragColor = vec4(TexCoord + 0.5, 0.0, 1.0);
-    FragColor = texture(ourTexture, TexCoord) * vec4(vec3(1.0) * normal.x * 1 + 0.5, 1.0);
+    vec3 n = normalize(normal);
+vec3 lightDir = vec3(1,0,0);
+    vec3 l = normalize(-lightDir);
+float diff = max(dot(n, l), 0.0);
+    
+    vec3 ambient = vec3(0.1);
+vec3 diffuse = vec3(1.0) * diff;
+
+vec3 lighting = ambient + diffuse;
+
+    FragColor = texture(ourTexture, TexCoord) * vec4(lighting, 1.0);
 }
