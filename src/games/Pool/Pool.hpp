d989432215e2fe53986f1b58e826ad199ecd6e7b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-19 11:09:28 +0100

    Draw debug UI

--- src/games/Pool/Pool.hpp
+++ src/games/Pool/Pool.hpp
@@ -118,186 +118,187 @@ public:
   }
 
   void InitUI() override {
-    fe::SDLWindow *window = (fe::SDLWindow*)this->window.get();
-    const char* glsl_version = "#version 330 core";
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
+    // fe::SDLWindow *window = (fe::SDLWindow*)this->window.get();
+    // const char* glsl_version = "#version 330 core";
+    // IMGUI_CHECKVERSION();
+    // ImGui::CreateContext();
+    // io = ImGui::GetIO();
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
 
-    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsDark();
 
-    // ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplSDL3_InitForOpenGL(window->GetSDLWindow(), window->GetSDLGLContext());
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    // // ImGui_ImplGlfw_InitForOpenGL(window, true);
+    // ImGui_ImplSDL3_InitForOpenGL(window->GetSDLWindow(), window->GetSDLGLContext());
+    // ImGui_ImplOpenGL3_Init(glsl_version);
   }
 
   void DrawUI() override {
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL3_NewFrame();
-    ImGui::NewFrame();
+    DrawDebugUI();
+//     ImGui_ImplOpenGL3_NewFrame();
+//     ImGui_ImplSDL3_NewFrame();
+//     ImGui::NewFrame();
 
-    ImGui::Begin("Debug");
-    {
-      ImGui::Text("Hello, World!");
-      ImGui::Text("FPS %.1f", fpsCounter.deltaTime > 0.0 ? 1.0 / fpsCounter.deltaTime : 0.0);
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-      ImGui::Text("Objects: %zu", this->scene->GetObjects().size());
-      size_t totalVertices = 0;
-      for (auto& obj : this->scene->GetObjects())
-        for (auto& mesh : obj->meshes) totalVertices += mesh.GetVertices().size();
-      ImGui::Text("Vertices: %zu", totalVertices);
+//     ImGui::Begin("Debug");
+//     {
+//       ImGui::Text("Hello, World!");
+//       ImGui::Text("FPS %.1f", fpsCounter.deltaTime > 0.0 ? 1.0 / fpsCounter.deltaTime : 0.0);
+//       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+//       ImGui::Text("Objects: %zu", this->scene->GetObjects().size());
+//       size_t totalVertices = 0;
+//       for (auto& obj : this->scene->GetObjects())
+//         for (auto& mesh : obj->meshes) totalVertices += mesh.GetVertices().size();
+//       ImGui::Text("Vertices: %zu", totalVertices);
 
-      if (ImGui::Button("Enable VR", ImVec2(70, 20))) {
-        this->EnableXR ();
-      }
+//       if (ImGui::Button("Enable VR", ImVec2(70, 20))) {
+//         this->EnableXR ();
+//       }
 
-      if (ImGui::Button("Disable VR", ImVec2(70, 20))) {
-        this->DestroyXR();
-      }
+//       if (ImGui::Button("Disable VR", ImVec2(70, 20))) {
+//         this->DestroyXR();
+//       }
 
-      if (ImGui::Button("Enable AA", ImVec2(70, 20))) {
-        std::cout << "Button clicked!" << std::endl;
-      }
+//       if (ImGui::Button("Enable AA", ImVec2(70, 20))) {
+//         std::cout << "Button clicked!" << std::endl;
+//       }
 
-      static bool wireframe = false;
-      if (ImGui::Checkbox("Enable Wireframe", &wireframe)) {
-        if (wireframe) this->EnableWireframe();
-        else this->DisableWireframe();
-      }
+//       static bool wireframe = false;
+//       if (ImGui::Checkbox("Enable Wireframe", &wireframe)) {
+//         if (wireframe) this->EnableWireframe();
+//         else this->DisableWireframe();
+//       }
 
-      fe::Object* model = this->player.get();
-      ImGui::SliderFloat3("Position", &model->state.position.x, -10.0f, 10.0f);
-      for (size_t i = 0; i < this->npcs.size(); ++i) {
-        ImGui::Text("NPC %zu", i);
-        ImGui::SliderFloat3(("Position##npc" + std::to_string(i)).c_str(), &this->npcs[i]->state.position.x, -10.0f, 10.0f);
-        ImGui::SliderFloat3(("Rotation##npc" + std::to_string(i)).c_str(), &this->npcs[i]->state.rotation.x, -180.0f, 180.0f);
-      }
-    }
-    ImGui::End();
+//       fe::Object* model = this->player.get();
+//       ImGui::SliderFloat3("Position", &model->state.position.x, -10.0f, 10.0f);
+//       for (size_t i = 0; i < this->npcs.size(); ++i) {
+//         ImGui::Text("NPC %zu", i);
+//         ImGui::SliderFloat3(("Position##npc" + std::to_string(i)).c_str(), &this->npcs[i]->state.position.x, -10.0f, 10.0f);
+//         ImGui::SliderFloat3(("Rotation##npc" + std::to_string(i)).c_str(), &this->npcs[i]->state.rotation.x, -180.0f, 180.0f);
+//       }
+//     }
+//     ImGui::End();
 
-    ImGui::Begin("Objects");
-    {
-      static char filenameBuffer[512] = "\0";
-      static float newObjectScale = 1.0f;
+//     ImGui::Begin("Objects");
+//     {
+//       static char filenameBuffer[512] = "\0";
+//       static float newObjectScale = 1.0f;
 
-      ImGui::InputText("Model file (.obj)", filenameBuffer, IM_ARRAYSIZE(filenameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
-      ImGui::DragFloat3("Scale##newObj", &newObjectScale, 0.001f);
-      if (ImGui::Button("Load model")) {
-        LoadObj(filenameBuffer, newObjectScale);
-      }
-
-
-      static char mapNameBuffer[512] = "level.fes\0";
-      ImGui::InputText("Map file", mapNameBuffer, IM_ARRAYSIZE(mapNameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
-
-      if (ImGui::Button("Save map!"))
-        this->SaveLevel();
-
-      if (ImGui::Button("Load map!"))
-        this->LoadLevel();
-
-      if (ImGui::Button("Clear objects"))
-        this->scene->ClearObjects();
-
-      static bool snapToGrid = true;
-      ImGui::Checkbox("Snap to grid", &snapToGrid);
-      float step = snapToGrid ? 0.1f : 0.0001f;
-
-      size_t i = 0;
-      for (auto &object : scene->GetObjects()) {
-        ImGui::Text("Object %zu", i);
-        ImGui::DragFloat3(("Position##npc" + std::to_string(i)).c_str(), &object->state.position.x, step);
-        ImGui::DragFloat3(("Rotation##npc" + std::to_string(i)).c_str(), &object->state.rotation.x, step);
-        ImGui::DragFloat3(("Scale##npc" + std::to_string(i)).c_str(), &object->state.scale.x, step);
-        ++i;
-      }
-
-      // i = 0;
-      auto lights = scene->GetLights();
-      for (int i = 0; i < scene->GetLightCount(); ++i) {
-        ImGui::Text("Light %zu", i);
-        ImGui::DragFloat3(("Position##light" + std::to_string(i)).c_str(), &lights[i].position.x, step);
-        ImGui::DragFloat3(("Colour##light" + std::to_string(i)).c_str(), &lights[i].color.x, step);
-        ImGui::DragFloat(("Radius##light" + std::to_string(i)).c_str(), &lights[i].radius, step);
-        ImGui::DragFloat(("Intensity##light" + std::to_string(i)).c_str(), &lights[i].intensity, step);
-        ++i;
-      }
-    }
-    ImGui::End();
+//       ImGui::InputText("Model file (.obj)", filenameBuffer, IM_ARRAYSIZE(filenameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
+//       ImGui::DragFloat3("Scale##newObj", &newObjectScale, 0.001f);
+//       if (ImGui::Button("Load model")) {
+//         LoadObj(filenameBuffer, newObjectScale);
+//       }
 
 
+//       static char mapNameBuffer[512] = "level.fes\0";
+//       ImGui::InputText("Map file", mapNameBuffer, IM_ARRAYSIZE(mapNameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
 
-    ImGui::Begin("Multiplayer");
-    {
-      static char usernameBuffer[32] = "Bill\0";
-      static char addressBuffer[256] = "127.0.0.1\0";
-      int port = 2130;
+//       if (ImGui::Button("Save map!"))
+//         this->SaveLevel();
 
-      ImGui::InputText("Username", usernameBuffer, IM_ARRAYSIZE(usernameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
-      ImGui::InputText("Address", addressBuffer, IM_ARRAYSIZE(addressBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
-      ImGui::InputInt("Port", &port);
+//       if (ImGui::Button("Load map!"))
+//         this->LoadLevel();
 
-      if (ImGui::Button("Join", ImVec2(60, 0))) {
-        std::cout << "Connecting to server... " << addressBuffer << std::endl;
-        this->connectToServer(addressBuffer, port, usernameBuffer);
-      }
+//       if (ImGui::Button("Clear objects"))
+//         this->scene->ClearObjects();
 
-      fe::Object* model = this->player.get();
-      ImGui::SliderFloat3("Position", &model->state.position.x, -10.0f, 10.0f);
+//       static bool snapToGrid = true;
+//       ImGui::Checkbox("Snap to grid", &snapToGrid);
+//       float step = snapToGrid ? 0.1f : 0.0001f;
 
-      ImGui::Text("Players:");
-      for (auto& [id, client] : this->client->clientClients) {
-        ImGui::Text("Player #%i username: %s", id, client.username.c_str());
-      }
-    }
-    ImGui::End();
+//       size_t i = 0;
+//       for (auto &object : scene->GetObjects()) {
+//         ImGui::Text("Object %zu", i);
+//         ImGui::DragFloat3(("Position##npc" + std::to_string(i)).c_str(), &object->state.position.x, step);
+//         ImGui::DragFloat3(("Rotation##npc" + std::to_string(i)).c_str(), &object->state.rotation.x, step);
+//         ImGui::DragFloat3(("Scale##npc" + std::to_string(i)).c_str(), &object->state.scale.x, step);
+//         ++i;
+//       }
+
+//       // i = 0;
+//       auto lights = scene->GetLights();
+//       for (int i = 0; i < scene->GetLightCount(); ++i) {
+//         ImGui::Text("Light %zu", i);
+//         ImGui::DragFloat3(("Position##light" + std::to_string(i)).c_str(), &lights[i].position.x, step);
+//         ImGui::DragFloat3(("Colour##light" + std::to_string(i)).c_str(), &lights[i].color.x, step);
+//         ImGui::DragFloat(("Radius##light" + std::to_string(i)).c_str(), &lights[i].radius, step);
+//         ImGui::DragFloat(("Intensity##light" + std::to_string(i)).c_str(), &lights[i].intensity, step);
+//         ++i;
+//       }
+//     }
+//     ImGui::End();
+
+
+
+//     ImGui::Begin("Multiplayer");
+//     {
+//       static char usernameBuffer[32] = "Bill\0";
+//       static char addressBuffer[256] = "127.0.0.1\0";
+//       int port = 2130;
+
+//       ImGui::InputText("Username", usernameBuffer, IM_ARRAYSIZE(usernameBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
+//       ImGui::InputText("Address", addressBuffer, IM_ARRAYSIZE(addressBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
+//       ImGui::InputInt("Port", &port);
+
+//       if (ImGui::Button("Join", ImVec2(60, 0))) {
+//         std::cout << "Connecting to server... " << addressBuffer << std::endl;
+//         this->connectToServer(addressBuffer, port, usernameBuffer);
+//       }
+
+//       fe::Object* model = this->player.get();
+//       ImGui::SliderFloat3("Position", &model->state.position.x, -10.0f, 10.0f);
+
+//       ImGui::Text("Players:");
+//       for (auto& [id, client] : this->client->clientClients) {
+//         ImGui::Text("Player #%i username: %s", id, client.username.c_str());
+//       }
+//     }
+//     ImGui::End();
 
     
 
-    ImGui::Begin("Chat");
-    {
-      static char inputBuffer[256] = "";
-      ImGui::BeginChild("ChatHistory", ImVec2(0, -ImGui::GetFrameHeightWithSpacing() - 10), true, ImGuiWindowFlags_HorizontalScrollbar);
+//     ImGui::Begin("Chat");
+//     {
+//       static char inputBuffer[256] = "";
+//       ImGui::BeginChild("ChatHistory", ImVec2(0, -ImGui::GetFrameHeightWithSpacing() - 10), true, ImGuiWindowFlags_HorizontalScrollbar);
 
-      for (const auto& msg : messages) {
-        ImGui::TextWrapped("%s", msg.c_str());
-      }
+//       for (const auto& msg : messages) {
+//         ImGui::TextWrapped("%s", msg.c_str());
+//       }
 
-      // Auto-scroll to bottom if new messages
-      if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY()) {
-        ImGui::SetScrollHereY(1.0f);
-      }
+//       // Auto-scroll to bottom if new messages
+//       if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY()) {
+//         ImGui::SetScrollHereY(1.0f);
+//       }
 
-      ImGui::EndChild();
+//       ImGui::EndChild();
 
-      ImGui::Separator();
+//       ImGui::Separator();
 
-      ImGui::PushItemWidth(-70);
-      bool enter_pressed = ImGui::InputText("##Input", inputBuffer, IM_ARRAYSIZE(inputBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
-      ImGui::PopItemWidth();
+//       ImGui::PushItemWidth(-70);
+//       bool enter_pressed = ImGui::InputText("##Input", inputBuffer, IM_ARRAYSIZE(inputBuffer), ImGuiInputTextFlags_EnterReturnsTrue);
+//       ImGui::PopItemWidth();
 
-      ImGui::SameLine();
+//       ImGui::SameLine();
 
-      bool send_clicked = ImGui::Button("Send", ImVec2(60, 0));
+//       bool send_clicked = ImGui::Button("Send", ImVec2(60, 0));
 
-      if (send_clicked || enter_pressed) {
-        if (inputBuffer[0] != '\0') {
-          messages.push_back(std::string("You: ") + inputBuffer);
+//       if (send_clicked || enter_pressed) {
+//         if (inputBuffer[0] != '\0') {
+//           messages.push_back(std::string("You: ") + inputBuffer);
 
-#ifdef FE_WIN32
+// #ifdef FE_WIN32
 
-          client->sendMessage(inputBuffer);
-          #endif
+//           client->sendMessage(inputBuffer);
+//           #endif
 
-          inputBuffer[0] = '\0';
-          ImGui::SetKeyboardFocusHere(-1);
-        }
-      }
-    }
-    ImGui::End();
+//           inputBuffer[0] = '\0';
+//           ImGui::SetKeyboardFocusHere(-1);
+//         }
+//       }
+//     }
+//     ImGui::End();
 
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+//     ImGui::Render();
+//     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
   }
 };
