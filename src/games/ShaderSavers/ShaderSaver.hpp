64e0e8c840adb112bca25212009dfc5a1bb750ea
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-17 23:56:33 +0200

    Shader loading and use

--- src/games/ShaderSavers/ShaderSaver.hpp
+++ src/games/ShaderSavers/ShaderSaver.hpp
@@ -75,18 +75,26 @@ public:
     auto window = this->GetWindow<fe::SDLWindow>();
     window->DisableVSync();
 
-    const char *vertexShader = /* glsl */R"(
-      #version 330 core\nvoid main() {\
-        float x = -1.0 + float((gl_VertexID & 1) << 2);\
-        float y = -1.0 + float((gl_VertexID & 2) << 1);\
-        gl_Position = vec4(x, y, 0.0, 1.0);\
-      }
+    const char *vertexShader = R"(
+    #version 330 core
+    void main() {
+        float x = -1.0 + float((gl_VertexID & 1) << 2);
+        float y = -1.0 + float((gl_VertexID & 2) << 1);
+        gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+    )";
+
+    const char *fragmentShader = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
     )";
 
 
-    
-
-    LoadShaderTexts(vertexShader, );
+    LoadShaderTexts(vertexShader, fragmentShader);
+    shader->Use();
   
     GLuint vao;
     glGenVertexArrays(1, &vao);
