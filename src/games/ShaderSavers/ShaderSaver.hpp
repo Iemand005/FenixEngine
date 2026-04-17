91058c0a68ebd34b6e1f97ae129f2735fd4df255
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-17 23:47:12 +0200

    warp

--- src/games/ShaderSavers/ShaderSaver.hpp
+++ src/games/ShaderSavers/ShaderSaver.hpp
@@ -75,14 +75,18 @@ public:
     auto window = this->GetWindow<fe::SDLWindow>();
     window->DisableVSync();
 
-     const char *vertexShader = "#version 330 core\nvoid main() {\
+    const char *vertexShader = /* glsl */R"(
+      #version 330 core\nvoid main() {\
         float x = -1.0 + float((gl_VertexID & 1) << 2);\
         float y = -1.0 + float((gl_VertexID & 2) << 1);\
         gl_Position = vec4(x, y, 0.0, 1.0);\
-    }";
+      }
+    )";
 
-    LoadShaderTexts(vertexShader, "#version 330 core\n\
-      out vec4 FragColor; void main() { FragColor = vec4(1, 0, 0, 1); }");
+
+    
+
+    LoadShaderTexts(vertexShader, );
   
     GLuint vao;
     glGenVertexArrays(1, &vao);
