5d78864e369f74df3d4038ab19ef5efd198a7d67
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 01:59:11 +0200

    weight

--- src/games/ShaderSavers/ShaderSaver.hpp
+++ src/games/ShaderSavers/ShaderSaver.hpp
@@ -49,84 +49,83 @@ class ShaderSaver : public fe::Renderer {
 		}
 	}
 
-		bool Reload(const char* path, const char* vs, SDL_Time* out = nullptr) {
-			SDL_PathInfo i;
-			if (!SDL_GetPathInfo(path, &i)) return false;
+	bool Reload(const char* path, const char* vs, SDL_Time* out = nullptr) {
+		SDL_PathInfo i;
+		if (!SDL_GetPathInfo(path, &i)) return false;
 
-			try {
-				LoadShaders(fe::Shader::Vertex(vs), fe::Shader::Fragment(path));
-				shader->Use();
-			} catch (...) {
-				return false;
-			}
-
-			if (out) *out = i.modify_time;
-			return true;
+		try {
+			LoadShaders(fe::Shader::Vertex(vs), fe::Shader::Fragment(path));
+			shader->Use();
+		} catch (...) {
+			return false;
 		}
 
-		void Run(ScreenSaverMode mode = ScreenSaverMode::Window, HWND parent = nullptr) {
-			auto window = GetWindow<fe::SDLWindow>();
-			window->EnableVSync();
+		if (out) *out = i.modify_time;
+		return true;
+	}
 
-			if (mode == ScreenSaverMode::Preview) {
-				RECT r;
-				GetClientRect(parent, &r);
-				window->AttachToNativeParent(parent);
-				window->Resize(r.right - r.left, r.bottom - r.top);
-			} else if (mode == ScreenSaverMode::Fullscreen) {
-				window->GoBorderlessFullscreen();
-				window->Show();
-				SDL_HideCursor();
-				fullscreened = true;
-			} else
-				window->Show();
+	void Run(ScreenSaverMode mode = ScreenSaverMode::Window, HWND parent = nullptr) {
+		auto window = GetWindow<fe::SDLWindow>();
+		window->EnableVSync();
 
-			const char* vs = R"(
+		if (mode == ScreenSaverMode::Preview) {
+			RECT r;
+			GetClientRect(parent, &r);
+			window->AttachToNativeParent(parent);
+			window->Resize(r.right - r.left, r.bottom - r.top);
+		} else if (mode == ScreenSaverMode::Fullscreen) {
+			window->GoBorderlessFullscreen();
+			window->Show();
+			SDL_HideCursor();
+			fullscreened = true;
+		} else
+			window->Show();
+
+		const char* vs = R"(
         #version 330 core
         void main(){
             vec2 v[3]=vec2[3](vec2(-1,-1),vec2(3,-1),vec2(-1,3));
             gl_Position=vec4(v[gl_VertexID],0,1);
         })";
 
-			const char* fs = "E:\\FenixEngine\\src\\games\\ShaderSavers\\FragmentShader.glsl";
+		const char* fs = "E:\\FenixEngine\\src\\games\\ShaderSavers\\FragmentShader.glsl";
 
-			SDL_Time last = 0;
-			Reload(fs, vs, &last);
+		SDL_Time last = 0;
+		Reload(fs, vs, &last);
 
-			GLuint vao;
-			glGenVertexArrays(1, &vao);
-			glBindVertexArray(vao);
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-			GLint uTime = glGetUniformLocation(shader->getId(), "time");
-			GLint uRes = glGetUniformLocation(shader->getId(), "resolution");
+		GLint uTime = glGetUniformLocation(shader->getId(), "time");
+		GLint uRes = glGetUniformLocation(shader->getId(), "resolution");
+
+		SDL_GetWindowSize(window->GetSDLWindow(), &width, &height);
+		glViewport(0, 0, width, height);
+		SDL_GetMouseState(&startX, &startY);
+
+		while (!window->ShouldClose()) {
+			ProcessInput();
+
+			SDL_PathInfo i;
+			if (SDL_GetPathInfo(fs, &i) && i.modify_time > last) Reload(fs, vs, &last);
 
 			SDL_GetWindowSize(window->GetSDLWindow(), &width, &height);
 			glViewport(0, 0, width, height);
-			SDL_GetMouseState(&startX, &startY);
 
-			while (!window->ShouldClose()) {
-				ProcessInput();
+			shader->Use();
 
-				SDL_PathInfo i;
-				if (SDL_GetPathInfo(fs, &i) && i.modify_time > last) Reload(fs, vs, &last);
+			float t = SDL_GetTicks() * 0.001f;
 
-				SDL_GetWindowSize(window->GetSDLWindow(), &width, &height);
-				glViewport(0, 0, width, height);
+			if (uTime >= 0) glUniform1f(uTime, t);
+			if (uRes >= 0) glUniform2f(uRes, width, height);
 
-				shader->Use();
+			glBindVertexArray(vao);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
-				float t = SDL_GetTicks() * 0.001f;
-
-				if (uTime >= 0) glUniform1f(uTime, t);
-				if (uRes >= 0) glUniform2f(uRes, width, height);
-
-				glBindVertexArray(vao);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-
-				window->SwapBuffers();
-			}
-
-			Destroy();
+			window->SwapBuffers();
 		}
+
+		Destroy();
 	}
 };
