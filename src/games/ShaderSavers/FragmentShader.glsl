06f053cac58313a4c489020a92ec78880c018da8
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 00:33:20 +0200

    GACK

--- src/games/ShaderSavers/FragmentShader.glsl
+++ src/games/ShaderSavers/FragmentShader.glsl
@@ -5,14 +5,34 @@ out vec4 FragColor;
 uniform vec2 resolution;
 uniform float time;
 
-vec3 hsv2rgb(vec3 c) {
-    vec4 K = vec4(1.0, 2.0/3.0, 1.0/3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
+void main()
+{
+    vec2 fragCoord = gl_FragCoord.xy;
+    vec2 iResolution = resolution;
+    float iTime = time;
 
-void main() {
-    
-    // Output to screen
-    FragColor = vec4(vec3(time % 10, 0, 0),1.0);
+    vec2 pos = fragCoord / iResolution;
+
+    pos -= 0.5;
+    pos = abs(pos);
+
+    float lines = 40.0;
+    float speed = 2.1;
+    float shift = iTime * speed;
+
+    if (sqrt(pow(fragCoord.x - (iResolution.x * 0.5), 2.0) +
+             pow(fragCoord.y - (iResolution.y * 0.5), 2.0))
+        > iResolution.y * 0.5)
+    {
+        shift *= -1.0;
+    }
+
+    float aspectRatio = iResolution.x / iResolution.y;
+
+    vec2 thingie = pos * lines;
+    thingie.y /= aspectRatio;
+
+    float col = floor(mod(thingie.y + thingie.x + shift, 1.0) * 2.0);
+
+    FragColor = vec4(vec3(col), 1.0);
 }
