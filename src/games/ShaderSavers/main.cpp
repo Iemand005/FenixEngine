2e76668c225bd153c99423b69716786f55244ea5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-20 21:05:02 +0200

    alright hwnd catching

--- src/games/ShaderSavers/main.cpp
+++ src/games/ShaderSavers/main.cpp
@@ -9,6 +9,18 @@ int main(int argc, char* argv[]) {
     std::cout << "arg[" << i << "] = " << argv[i] << "\n";
   }
 
+  bool isPreview = false;
+  HWND previewHwnd = nullptr;
+
+  for (int i = 1; i < argc; i++)
+  {
+      if (strcmp(argv[i], "/p") == 0 && i + 1 < argc)
+      {
+          isPreview = true;
+          previewHwnd = (HWND)std::stoull(argv[i + 1]);
+      }
+  }
+
   ShaderSaver game;
   game.Run();
   return 0;
