321a3cf88045b6f1e6a09e19f5573026c47792d7
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 02:35:34 +0200

    now

--- src/Engine/window/GLFW3Window.hpp
+++ src/Engine/window/GLFW3Window.hpp
@@ -1,164 +1,152 @@
 #pragma once
 
-#include <iostream>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 #include "IWindow.hpp"
 
 namespace fe {
 
-	class GLFW3Window : public IWindow {
-		private:
-		
-		struct Impl;
-		std::unique_ptr<Impl> impl;
-		std::string title;
+class GLFW3Window : public IWindow {
+   private:
+	struct Impl;
+	std::unique_ptr<Impl> impl;
+	std::string title;
 
-    public:
+   public:
+	GLFW3Window(std::string title, int width, int height, bool hidden = false) : IWindow(width, height), title(title) { InitGlfw(); }
 
-    GLFW3Window(std::string title, int width, int height, bool hidden = false) : IWindow(width, height), title(title) {
-      InitGlfw();
-    }
-
-bool InitGlfw(bool tenBit = false) {
+	bool InitGlfw(bool tenBit = false) {
 #ifdef WAYLAND
-    if (glfwPlatformSupported(GLFW_PLATFORM_WAYLAND)) {
-      glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
-    } else {
-      std::cerr << "No Wayland Support" << std::endl;
-    }
+		if (glfwPlatformSupported(GLFW_PLATFORM_WAYLAND)) {
+			glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
+		} else {
+			std::cerr << "No Wayland Support" << std::endl;
+		}
 #endif
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (tenBit) {
-      glfwWindowHint(GLFW_RED_BITS, 10);
-      glfwWindowHint(GLFW_GREEN_BITS, 10);
-      glfwWindowHint(GLFW_BLUE_BITS, 10);
-      glfwWindowHint(GLFW_ALPHA_BITS, 2);
-    }
+		if (tenBit) {
+			glfwWindowHint(GLFW_RED_BITS, 10);
+			glfwWindowHint(GLFW_GREEN_BITS, 10);
+			glfwWindowHint(GLFW_BLUE_BITS, 10);
+			glfwWindowHint(GLFW_ALPHA_BITS, 2);
+		}
 
-    impl->window = glfwCreateWindow(width, height, "FoxEngine", NULL, NULL);
-    if (impl->window == NULL) {
-      std::cout << "Failed to create GLFW window" << std::endl;
-      glfwTerminate();
-      return false;
-    }
-    glfwMakeContextCurrent(impl->window);
+		impl->window = glfwCreateWindow(width, height, "FoxEngine", NULL, NULL);
+		if (impl->window == NULL) {
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return false;
+		}
+		glfwMakeContextCurrent(impl->window);
 
-    // glfwSwapInterval(vsync ? 1 : 0);  // Enable vsync
-    EnableVSync();
+		// glfwSwapInterval(vsync ? 1 : 0);  // Enable vsync
+		EnableVSync();
 
-    // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    //   std::cout << "Failed to initialize GLAD" << std::endl;
-    //   return false;
-    // }
+		// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		//   std::cout << "Failed to initialize GLAD" << std::endl;
+		//   return false;
+		// }
 
-    glfwSetWindowUserPointer(impl->window, this);
+		glfwSetWindowUserPointer(impl->window, this);
 
-    // glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yOffset) {
-    //   auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
-    //   ImGuiIO& io = ImGui::GetIO();
-    //   if (io.WantCaptureMouse) return;
-    //   game->fov -= (float)yOffset;
-    //   if (game->fov < 1.0f) game->fov = 1.0f;
-    //   if (game->fov > 45.0f) game->fov = 45.0f;
-    // });
-    // glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
-    //   ImGuiIO& io = ImGui::GetIO();
-    //   if (io.WantCaptureMouse) return;
-    // });
+		// glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yOffset) {
+		//   auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
+		//   ImGuiIO& io = ImGui::GetIO();
+		//   if (io.WantCaptureMouse) return;
+		//   game->fov -= (float)yOffset;
+		//   if (game->fov < 1.0f) game->fov = 1.0f;
+		//   if (game->fov > 45.0f) game->fov = 45.0f;
+		// });
+		// glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+		//   ImGuiIO& io = ImGui::GetIO();
+		//   if (io.WantCaptureMouse) return;
+		// });
 
-    glfwSetCursorPosCallback(impl->window, [](GLFWwindow* window, double xPos, double yPos) {
-      if (!(glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)) {
-        // ImGui::SetNextFrameWantCaptureMouse(false);
-        return;
-      }
+		glfwSetCursorPosCallback(impl->window, [](GLFWwindow* window, double xPos, double yPos) {
+			if (!(glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)) {
+				// ImGui::SetNextFrameWantCaptureMouse(false);
+				return;
+			}
 
-      // auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
-      // ImGuiIO& io = ImGui::GetIO();
-      // if (io.WantCaptureMouse) return;
+			// auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
+			// ImGuiIO& io = ImGui::GetIO();
+			// if (io.WantCaptureMouse) return;
 
-      // float xOffset = xPos - game->lastX;
-      // float yOffset = game->lastY - yPos;
-      // if (game->lastX == 0 && game->lastY == 0) {
-      //   xOffset = 0;
-      //   yOffset = 0;
-      // }
-      // game->lastX = xPos;
-      // game->lastY = yPos;
+			// float xOffset = xPos - game->lastX;
+			// float yOffset = game->lastY - yPos;
+			// if (game->lastX == 0 && game->lastY == 0) {
+			//   xOffset = 0;
+			//   yOffset = 0;
+			// }
+			// game->lastX = xPos;
+			// game->lastY = yPos;
 
-      // const float sensitivity = 0.1f;
-      // xOffset *= sensitivity;
-      // yOffset *= sensitivity;
+			// const float sensitivity = 0.1f;
+			// xOffset *= sensitivity;
+			// yOffset *= sensitivity;
 
-      // game->yaw += xOffset;
-      // game->pitch += yOffset;
+			// game->yaw += xOffset;
+			// game->pitch += yOffset;
 
-      // if (game->pitch > 89.0f) game->pitch = 89.0f;
-      // if (game->pitch < -89.0f) game->pitch = -89.0f;
+			// if (game->pitch > 89.0f) game->pitch = 89.0f;
+			// if (game->pitch < -89.0f) game->pitch = -89.0f;
 
-      // glm::vec3 direction;
-      // direction.x = cos(glm::radians(game->yaw)) * cos(glm::radians(game->pitch));
-      // direction.y = sin(glm::radians(game->pitch));
-      // direction.z = sin(glm::radians(game->yaw)) * cos(glm::radians(game->pitch));
-      // game->camera->front = glm::normalize(direction);
-    });
+			// glm::vec3 direction;
+			// direction.x = cos(glm::radians(game->yaw)) * cos(glm::radians(game->pitch));
+			// direction.y = sin(glm::radians(game->pitch));
+			// direction.z = sin(glm::radians(game->yaw)) * cos(glm::radians(game->pitch));
+			// game->camera->front = glm::normalize(direction);
+		});
 
-    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
-      // auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
-      // // game->width = width;
-      // // game->height = height;
-      // // game->scene->resize(width, height);
+		glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+			// auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
+			// // game->width = width;
+			// // game->height = height;
+			// // game->scene->resize(width, height);
 
-      // // game->updateAspect();
-      // game->Resize(width, height);
+			// // game->updateAspect();
+			// game->Resize(width, height);
 
-      // game->Redraw();
-    });
-    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
-      // auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
-      // game->Resize(width, height);
-      // game->Redraw();
-    });
+			// game->Redraw();
+		});
+		glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+			// auto game = static_cast<Game*>(glfwGetWindowUserPointer(window));
+			// game->Resize(width, height);
+			// game->Redraw();
+		});
 
+		// glfwGetWindowAttrib(window, GLFW_TOUCH);
+		return true;
+	}
 
-    // glfwGetWindowAttrib(window, GLFW_TOUCH);
-    return true;
-  }
+	void StartMouseCapture() override {
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		// io.WantCaptureMouse = false;
+	}
 
-  void StartMouseCapture() override {
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    // io.WantCaptureMouse = false;
-  }
+	void StopMouseCapture() override {
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		// io.WantCaptureMouse = true;
+	}
 
-  void StopMouseCapture() override {
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    // io.WantCaptureMouse = true;
+	void PollGLFWEvents() { glfwPollEvents(); }
 
-  }
+	void SetSwapInterval(int interval) override { glfwSwapInterval(interval); }
 
-  void PollGLFWEvents() {
-    glfwPollEvents();
-  }
+	void SwapBuffers() override { glfwSwapBuffers(this->window); }
 
-  void SetSwapInterval(int interval) override {
-    glfwSwapInterval(interval);
-  }
+	void Destroy() override {
+		glfwDestroyWindow(this->window);
+		glfwTerminate();
+	}
 
-  void SwapBuffers() override {
-    glfwSwapBuffers(this->window);
-  }
-
-  void Destroy() override {
-    glfwDestroyWindow(this->window);
-    glfwTerminate();
-  }
-
-  void *GetGLFWWindow() {
-    return window;
-  }
+	void* GetGLFWWindow() { return window; }
 };
-}
+}  // namespace fe
