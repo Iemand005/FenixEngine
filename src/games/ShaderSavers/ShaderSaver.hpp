2f1aa4db03eb87902dcbfd03e44711bb29f0057a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 04:08:15 +0200

    im a rockstar

--- src/games/ShaderSavers/ShaderSaver.hpp
+++ src/games/ShaderSavers/ShaderSaver.hpp
@@ -125,11 +125,7 @@ public:
 		const char* vs = R"(
 			#version 330 core
 			void main() {
-				vec2 v[3]=vec2[3](
-					vec2(-1,-1),
-					vec2(3,-1),
-					vec2(-1,3)
-				);
+				vec2 v[3]=vec2[3](vec2(-1,-1), vec2(3,-1), vec2(-1,3));
 				gl_Position=vec4(v[gl_VertexID],0,1);
 			})";
 
