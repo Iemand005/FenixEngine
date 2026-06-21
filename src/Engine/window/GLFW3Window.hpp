2f9afa6d9a638717e42929fb12cc41547f309f15
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 02:52:48 +0200

    where

--- src/Engine/window/GLFW3Window.hpp
+++ src/Engine/window/GLFW3Window.hpp
@@ -1,36 +1,36 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
-
 #include <iostream>
-
+#include <memory>
 #include "IWindow.hpp"
 
 namespace fe {
-	class GLFW3Window : public IWindow {
-	public:
-		GLFW3Window(std::string title, int width, int height, bool hidden = false);
-		~GLFW3Window();
-		
-		bool InitGlfw(bool tenBit = false);
-		
-		void StartMouseCapture() override;
-		
-		void StopMouseCapture() override;
-		
-		void PollGLFWEvents() { glfwPollEvents(); }
-		
-		void SetSwapInterval(int interval) override { glfwSwapInterval(interval); }
-		
-		void SwapBuffers() override;
-		
-		void Destroy() override;
-		
-		void* GetGLFWWindow();
-		
-	private:
-		struct Impl;
-		std::unique_ptr<Impl> impl;
-		std::string title;
-	};
-}  // namespace fe
+
+class GLFW3Window : public IWindow {
+public:
+	GLFW3Window(std::string title, int width, int height, bool hidden = false);
+	~GLFW3Window();
+
+	bool InitGlfw(bool tenBit = false);
+
+	void StartMouseCapture() override;
+	void StopMouseCapture() override;
+
+	void PollGLFWEvents() { glfwPollEvents(); }
+
+	void SetSwapInterval(int interval) override { glfwSwapInterval(interval); }
+
+	void SwapBuffers() override;
+
+	void Destroy() override;
+
+	void* GetGLFWWindow();
+
+private:
+	struct Impl;
+	std::unique_ptr<Impl> impl;
+	std::string title;
+};
+
+} // namespace fe
