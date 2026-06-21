141b64459529191791f56b634c0a6334ff71f12b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 02:44:38 +0200

    rprivate down

--- src/Engine/window/GLFW3Window.hpp
+++ src/Engine/window/GLFW3Window.hpp
@@ -8,28 +8,29 @@
 
 namespace fe {
 	class GLFW3Window : public IWindow {
+		
+		public:
+		GLFW3Window(std::string title, int width, int height, bool hidden = false);
+		~GLFW3Window();
+		
+		bool InitGlfw(bool tenBit = false);
+		
+		void StartMouseCapture() override;
+		
+		void StopMouseCapture() override;
+		
+		void PollGLFWEvents() { glfwPollEvents(); }
+		
+		void SetSwapInterval(int interval) override { glfwSwapInterval(interval); }
+		
+		void SwapBuffers() override;
+		
+		void Destroy() override;
+		
+		void* GetGLFWWindow();
 	private:
 		struct Impl;
 		std::unique_ptr<Impl> impl;
 		std::string title;
-
-	public:
-		GLFW3Window(std::string title, int width, int height, bool hidden = false);
-
-		bool InitGlfw(bool tenBit = false);
-
-		void StartMouseCapture() override;
-
-		void StopMouseCapture() override;
-
-		void PollGLFWEvents() { glfwPollEvents(); }
-
-		void SetSwapInterval(int interval) override { glfwSwapInterval(interval); }
-
-		void SwapBuffers() override;
-
-		void Destroy() override;
-
-		void* GetGLFWWindow();
 	};
 }  // namespace fe
