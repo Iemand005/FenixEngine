ebad1c9464ad6fe83c2ec927dc9fc88d6ae616e5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-23 16:16:18 +0200

    ptr yah

--- src/games/AudioTest/AudioTest.hpp
+++ src/games/AudioTest/AudioTest.hpp
@@ -84,7 +84,7 @@ public:
 		for (int i = 0; i < NUM_BARS; ++i) {
 			auto cube = std::make_shared<fe::Object>(fe::Primitives::GenerateCube(1.0f));
 
-			cube.state.position = glm::vec3(-15.0f + i * 1.0f, 0.0f, -5.0f);
+			cube->state.position = glm::vec3(-15.0f + i * 1.0f, 0.0f, -5.0f);
 	
 			this->scene->AddObject(cube);
 
