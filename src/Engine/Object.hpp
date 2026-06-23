3005fbb63f420406c6b75c9f128bf4644ab783b0
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-23 16:09:50 +0200

    yes ok

--- src/Engine/Object.hpp
+++ src/Engine/Object.hpp
@@ -61,7 +61,7 @@ class Object {
 
   Object() {
     //acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-    state.scale = glm::vec3(1.0f); // ok? glm::vec3();
+    state.scale = glm::vec3(1.0f);
     meshes = std::vector<Mesh>();
   }
 
