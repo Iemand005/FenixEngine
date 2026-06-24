306440717a89379a22f4d7bc5c4311e3f03d5f3a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 21:40:37 +0200

    av

--- src/games/Cake/Cake.hpp
+++ src/games/Cake/Cake.hpp
@@ -75,7 +75,7 @@ public:
 		auto planeMesh = fe::Primitives::GeneratePlane();
 		planeMesh.loadTexture("resources/textures/cake_top.png", fe::TextureScaling::Nearest);
 		constexpr float sideInset = 16.0f / 14.0f;
-		auto sideMesh = fe::Primitives::GenerateCube({fe::PlaneDirection::Front, fe::PlaneDirection::Left, fe::PlaneDirection::Right, fe::PlaneDirection::Back}, 1, 0);
+		auto sideMesh = fe::Primitives::GenerateCube({fe::PlaneDirection::Front, fe::PlaneDirection::Left, fe::PlaneDirection::Right, fe::PlaneDirection::Back}, 1, sideInset);
 		sideMesh.loadTexture("resources/textures/cake_side.png", fe::TextureScaling::Nearest);
 
 		auto plane = std::make_shared<fe::Object>(planeMesh);
