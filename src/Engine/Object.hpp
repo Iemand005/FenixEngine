dbfedf44452ad1ddfcc524688747703bc2db7317
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-25 00:42:03 +0200

    180 then huh

--- src/Engine/Object.hpp
+++ src/Engine/Object.hpp
@@ -123,7 +123,7 @@ class Object {
 		float pitch = glm::degrees(asin(direction.y));
 		float yaw = glm::degrees(atan2(direction.x, direction.z));
 		this->state.rotation.x = pitch;
-		this->state.rotation.y = yaw;
+		this->state.rotation.y = yaw - 180;
 	}
 
   std::string GetName() {
