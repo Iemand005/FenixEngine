55129ec30edd45217180b9ef852a7aa9469532bc
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 20:58:25 +0200

    use tabs

--- src/Engine/Game.hpp
+++ src/Engine/Game.hpp
@@ -38,35 +38,35 @@
 namespace fe {
 
 class Game : public Renderer {
- public:
-  // std::unique_ptr<IWindow> window = nullptr;
-  // std::unique_ptr<Scene> scene;
-  // std::unique_ptr<Camera> camera;
-  // std::unique_ptr<ShaderProgram> shader;
-  // fe::Timer fpsCounter;
+public:
+	// std::unique_ptr<IWindow> window = nullptr;
+	// std::unique_ptr<Scene> scene;
+	// std::unique_ptr<Camera> camera;
+	// std::unique_ptr<ShaderProgram> shader;
+	// fe::Timer fpsCounter;
 
-  float yaw = -90.0f, pitch = 0.0f;
+	float yaw = -90.0f, pitch = 0.0f;
 
-  int lastX, lastY;
+	int lastX, lastY;
 
-  bool capturingMouse = true;
+	bool capturingMouse = true;
 
-  std::shared_ptr<Character> player;
+	std::shared_ptr<Character> player;
 
-  std::vector<std::shared_ptr<Character>> npcs = std::vector<std::shared_ptr<Character>>();
+	std::vector<std::shared_ptr<Character>> npcs = std::vector<std::shared_ptr<Character>>();
 
-  std::vector<std::shared_ptr<Object>> maps = std::vector<std::shared_ptr<Object>>();
+	std::vector<std::shared_ptr<Object>> maps = std::vector<std::shared_ptr<Object>>();
 
-  std::vector<std::string> messages;
+	std::vector<std::string> messages;
 
-  double lastUpdateTime = 0.0f;
+	double lastUpdateTime = 0.0f;
 
-  bool canJump = true;
+	bool canJump = true;
 
-  int mapIndex = 0;
+	int mapIndex = 0;
 
 #ifndef EXCLUDE_NETWORKING
-  std::unique_ptr<Networker> client = nullptr;
+	std::unique_ptr<Networker> client = nullptr;
 #endif
 
 	std::unordered_map<unsigned char, std::shared_ptr<Character>> players =
@@ -110,158 +110,158 @@ class Game : public Renderer {
 		InitUI();
 	}
 
-  void Log(const std::string& message) { std::cout << message << std::endl; }
+	void Log(const std::string& message) { std::cout << message << std::endl; }
 
-  void LoadShaders(std::string vertexShaderPath, std::string fragmentShaderPath) {
-    this->shader = std::make_unique<fe::ShaderProgram>(vertexShaderPath, fragmentShaderPath);
-  }
+	void LoadShaders(std::string vertexShaderPath, std::string fragmentShaderPath) {
+		this->shader = std::make_unique<fe::ShaderProgram>(vertexShaderPath, fragmentShaderPath);
+	}
 
-  bool LoadShaderTexts(std::string vertexShaderText, std::string fragmentShaderText) {
-    this->shader = std::make_unique<fe::ShaderProgram>();
-    return this->shader->LoadShaderTexts(vertexShaderText, fragmentShaderText);
-  }
+	bool LoadShaderTexts(std::string vertexShaderText, std::string fragmentShaderText) {
+		this->shader = std::make_unique<fe::ShaderProgram>();
+		return this->shader->LoadShaderTexts(vertexShaderText, fragmentShaderText);
+	}
 
-  void MovePlayer(Direction direction) {
-    this->player->Move(direction, camera.get());
-  }
+	void MovePlayer(Direction direction) {
+		this->player->Move(direction, camera.get());
+	}
 
-  void MoveCamera(Direction direction, float dt = 1.0f) {
-    this->camera->Move(direction, dt);
-  }
+	void MoveCamera(Direction direction, float dt = 1.0f) {
+		this->camera->Move(direction, dt);
+	}
 
-  void MouseMove(int x, int y) {
-    const float sensitivity = 0.1f;
+	void MouseMove(int x, int y) {
+		const float sensitivity = 0.1f;
 
-    this->yaw += sensitivity * x;
-    this->pitch += sensitivity * -y;
+		this->yaw += sensitivity * x;
+		this->pitch += sensitivity * -y;
 
-    if (this->pitch > 89.0f) this->pitch = 89.0f;
-    if (this->pitch < -89.0f) this->pitch = -89.0f;
+		if (this->pitch > 89.0f) this->pitch = 89.0f;
+		if (this->pitch < -89.0f) this->pitch = -89.0f;
 
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
-    direction.y = sin(glm::radians(this->pitch));
-    direction.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
-    this->camera->setFront(glm::normalize(direction));
-  }
+		glm::vec3 direction;
+		direction.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+		direction.y = sin(glm::radians(this->pitch));
+		direction.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+		this->camera->setFront(glm::normalize(direction));
+	}
 
-  void SaveLevel(std::string fileName = "level.fes") {
-    this->level->Save(this->scene->GetFilteredObjects(player), fileName);
-  }
+	void SaveLevel(std::string fileName = "level.fes") {
+		this->level->Save(this->scene->GetFilteredObjects(player), fileName);
+	}
 
-  void LoadLevel(std::string fileName = "level.fes") {
-    auto objects = this->level->Load(fileName);
-    this->scene->ClearObjects();
-    this->scene->AddObject(player);
-    for (auto& object : objects) this->scene->AddObject(object);
-  }
+	void LoadLevel(std::string fileName = "level.fes") {
+		auto objects = this->level->Load(fileName);
+		this->scene->ClearObjects();
+		this->scene->AddObject(player);
+		for (auto& object : objects) this->scene->AddObject(object);
+	}
 
 #ifndef EXCLUDE_NETWORKING
-  void connectToServer(std::string address, unsigned short port, std::string username) {
-    this->client->Connect(address, port, username);
-  }
+	void connectToServer(std::string address, unsigned short port, std::string username) {
+		this->client->Connect(address, port, username);
+	}
 #endif
 
-  void loadMap(int index) {
-    auto map = maps.at(index);
-    scene->GetObjects()[0] = map;
+	void loadMap(int index) {
+		auto map = maps.at(index);
+		scene->GetObjects()[0] = map;
 
-    for (auto& mesh : map->meshes) {
-      auto vertices = std::vector<glm::vec3>();
-      for (auto& vertex : mesh.vertices) vertices.push_back(vertex.position);
-      mesh.SetPhysicsObject(physicsEngine->CreateObject(vertices, mesh.indices));
-    }
-  }
+		for (auto& mesh : map->meshes) {
+			auto vertices = std::vector<glm::vec3>();
+			for (auto& vertex : mesh.vertices) vertices.push_back(vertex.position);
+			mesh.SetPhysicsObject(physicsEngine->CreateObject(vertices, mesh.indices));
+		}
+	}
 
-  void nextMap() {
-    loadMap(mapIndex);
-    mapIndex++;
-    if (mapIndex >= maps.size()) mapIndex = 0;
-  }
+	void nextMap() {
+		loadMap(mapIndex);
+		mapIndex++;
+		if (mapIndex >= maps.size()) mapIndex = 0;
+	}
 
-  void SpawnPlayer(unsigned char playerId) {
-    auto newPlayer = std::static_pointer_cast<fe::Character>(this->player->Clone());
+	void SpawnPlayer(unsigned char playerId) {
+		auto newPlayer = std::static_pointer_cast<fe::Character>(this->player->Clone());
 
-    this->players.insert_or_assign(playerId, newPlayer);
+		this->players.insert_or_assign(playerId, newPlayer);
 
-    this->scene->AddObject(newPlayer);
-  }
+		this->scene->AddObject(newPlayer);
+	}
 
-  std::shared_ptr<fe::Object> LoadObj(std::string path, float scale = 1.0f) {
-    std::shared_ptr<fe::Object> model = std::make_shared<fe::Object>(path, scale);
-    this->scene->AddObject(model);
-    return model;
-  }
+	std::shared_ptr<fe::Object> LoadObj(std::string path, float scale = 1.0f) {
+		std::shared_ptr<fe::Object> model = std::make_shared<fe::Object>(path, scale);
+		this->scene->AddObject(model);
+		return model;
+	}
 
-  std::shared_ptr<fe::Object> loadOBJButDontAdd(std::string path, float scale = 1.0f) {
-    return std::make_shared<fe::Object>(path, scale);
-  }
+	std::shared_ptr<fe::Object> loadOBJButDontAdd(std::string path, float scale = 1.0f) {
+		return std::make_shared<fe::Object>(path, scale);
+	}
 
-  std::shared_ptr<fe::Object> LoadStaticOBJ(std::string path, float scale = 1.0f) {
-    std::shared_ptr<fe::Object> model = std::make_shared<fe::Object>(path, scale);
-    model->isStatic = true;
-    return model;
-  }
+	std::shared_ptr<fe::Object> LoadStaticOBJ(std::string path, float scale = 1.0f) {
+		std::shared_ptr<fe::Object> model = std::make_shared<fe::Object>(path, scale);
+		model->isStatic = true;
+		return model;
+	}
 
-  double getDeltaTime() { return 1; }
+	double getDeltaTime() { return 1; }
 
-  void SetClearColor(float r, float g, float b, float a = 1);
+	void SetClearColor(float r, float g, float b, float a = 1);
 
-  void Resize() { Resize(this->window->width, this->window->height); }
-  void Resize(int width, int height) {
-    if (this->scene) this->scene->Resize(width, height);
-    this->UpdateAspect(width, height);
-  }
+	void Resize() { Resize(this->window->width, this->window->height); }
+	void Resize(int width, int height) {
+		if (this->scene) this->scene->Resize(width, height);
+		this->UpdateAspect(width, height);
+	}
 
-  void Redraw(GLuint fbo) {
-    BindFrameBuffer(fbo);
-    Redraw();
-  }
+	void Redraw(GLuint fbo) {
+		BindFrameBuffer(fbo);
+		Redraw();
+	}
 
-  void Redraw();
+	void Redraw();
 
-  void CheckErrors();
+	void CheckErrors();
 
-  void Update() {
-    double dt = scene->Update();
-    UpdatePhysics(dt);
-  }
+	void Update() {
+		double dt = scene->Update();
+		UpdatePhysics(dt);
+	}
 
-  void UpdatePhysics(double deltaTime) {
-    if (physicsEngine) physicsEngine->Update(deltaTime);
-  }
+	void UpdatePhysics(double deltaTime) {
+		if (physicsEngine) physicsEngine->Update(deltaTime);
+	}
 
-  virtual void InitUI() {}
-  virtual void DrawUI() {}
-  virtual void OnDraw() {}
+	virtual void InitUI() {}
+	virtual void DrawUI() {}
+	virtual void OnDraw() {}
 
-  void EnableWireframe();
-  void DisableWireframe();
-  void ToggleWireframe(bool enabled = false);
+	void EnableWireframe();
+	void DisableWireframe();
+	void ToggleWireframe(bool enabled = false);
 
-  template<typename WindowT = IWindow>
-  WindowT* GetWindow() {
-    return (WindowT*)this->window.get();
-  }
+	template<typename WindowT = IWindow>
+	WindowT* GetWindow() {
+		return (WindowT*)this->window.get();
+	}
 
-  double GetFPS() {
-    return fpsCounter.deltaTime > 0.0 ? 1.0 / fpsCounter.deltaTime : 0.0;
-  }
+	double GetFPS() {
+		return fpsCounter.deltaTime > 0.0 ? 1.0 / fpsCounter.deltaTime : 0.0;
+	}
 
-  void BindFrameBuffer(int bufferIndex = 0);
+	void BindFrameBuffer(int bufferIndex = 0);
 
-  void UpdateAspect(int width, int height) {
-    if (this->camera) this->camera->SetAspect(width, height);
-  }
+	void UpdateAspect(int width, int height) {
+		if (this->camera) this->camera->SetAspect(width, height);
+	}
 
-  bool ShouldClose() {
-    if (!this->window) return false;
-    return this->window->ShouldClose();
-  }
+	bool ShouldClose() {
+		if (!this->window) return false;
+		return this->window->ShouldClose();
+	}
 
-  void Destroy() {
-    if (window) window->Destroy();
-  }
+	void Destroy() {
+		if (window) window->Destroy();
+	}
 };
 
 }
