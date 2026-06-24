cca844baa14fb89f9e7eec03ea7abaeee7b0400e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 21:58:40 +0200

    since it's an outset we name it that

--- src/games/Cake/Cake.hpp
+++ src/games/Cake/Cake.hpp
@@ -75,10 +75,13 @@ public:
 		auto planeMesh = fe::Primitives::GeneratePlane();
 		planeMesh.loadTexture("resources/textures/cake_top.png", fe::TextureScaling::Nearest);
 		planeMesh.hasTransparency = true;
+		
 		constexpr float sideInset = 15.0f / 16.0f;
 		auto sideMesh = fe::Primitives::GenerateCube({fe::PlaneDirection::Front, fe::PlaneDirection::Left, fe::PlaneDirection::Right, fe::PlaneDirection::Back}, 1, -sideInset);
 		sideMesh.loadTexture("resources/textures/cake_side.png", fe::TextureScaling::Nearest);
 		sideMesh.hasTransparency = true;
+
+
 
 		auto plane = std::make_shared<fe::Object>(planeMesh);
 		plane->meshes.push_back(sideMesh);
