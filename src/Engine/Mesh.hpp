b12317cf3c2727f16f3024301db7ecbdbf8842d1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 21:14:52 +0200

    default linear

--- src/Engine/Mesh.hpp
+++ src/Engine/Mesh.hpp
@@ -160,6 +160,10 @@ namespace fe {
 
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+			GLint texScaling = GL_LINEAR;
+			if (scaling == TextureScaling::Linear)
+
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
