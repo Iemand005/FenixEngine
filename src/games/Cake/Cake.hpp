90ed6ad0979e1e80aceb4f42cfdd3642c68939a5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 23:37:30 +0200

    that's a no

--- src/games/Cake/Cake.hpp
+++ src/games/Cake/Cake.hpp
@@ -106,7 +106,7 @@ public:
 		sideUV.u0 = 0;
 		sideUV.u1 = 2.0f / 16.0f;
 		sideUV.v0 = 0;
-		sideUV.v1 = 0.8f / 16.0f;
+		sideUV.v1 = 8.09f / 16.0f;
 
 		candleUVs.top = topUV;
 		candleUVs.bottom = topUV;
