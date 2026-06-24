23997b93c48fae13a6ab94fae59171ba311829ed
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 22:40:06 +0200

    inset but 1 - ofc my badd form tehs tart

--- src/games/Cake/Cake.hpp
+++ src/games/Cake/Cake.hpp
@@ -83,7 +83,7 @@ public:
 		planeMesh.loadTexture("resources/textures/cake_top.png", fe::TextureScaling::Nearest);
 		planeMesh.hasTransparency = true;
 
-		constexpr float sideInset = 15.0f / 16.0f;
+		constexpr float sideInset = 1 - 15.0f / 16.0f;
 		auto sideMesh = fe::Primitives::GenerateCube({fe::PlaneDirection::Front, fe::PlaneDirection::Left, fe::PlaneDirection::Right, fe::PlaneDirection::Back}, 1, -sideInset);
 		sideMesh.loadTexture("resources/textures/cake_side.png", fe::TextureScaling::Nearest);
 		sideMesh.hasTransparency = true;
