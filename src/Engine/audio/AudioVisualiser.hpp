bf8ad21df4c3136cb3aa1590152108997670aef1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-24 16:56:57 +0200

    visualizer init

--- src/Engine/audio/AudioVisualiser.hpp
+++ src/Engine/audio/AudioVisualiser.hpp
@@ -1,4 +1,10 @@
 
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
 #include "kiss_fftr.h"
 
 #ifdef _WIN32
@@ -7,6 +13,20 @@
 #include <audio/PipeWireLoopbackCapture.hpp>
 #endif
 
+std::vector<float> audioSamples;         
+kiss_fftr_cfg      fftConfig;            
+float              fftInput[FFT_SIZE];  
+kiss_fft_cpx       fftOutput[BINS]; 
+
 class AudioVisualiser {
+public:
+	void Init() {
+		fftConfig = kiss_fftr_alloc(FFT_SIZE, 0, nullptr, nullptr);
+#ifdef _WIN32
+		g_loopback.Init();
+#else
+		g_pwLoopback.Init();
+#endif
+	}
 
 };
