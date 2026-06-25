30f597dc700af473bac77fdc495dee54c3272e3d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-25 17:35:56 +0200

    kak need good solution tho

--- src/Engine/Shader.hpp
+++ src/Engine/Shader.hpp
@@ -1,8 +1,3 @@
-#pragma once
-#ifdef _WIN32
-#define NOMINMAX
-#include <Windows.h>
-#endif
 
 #include <glad/glad.h>
 
@@ -18,7 +13,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "WavaDir.hpp"
+#include "WawaDir.hpp"
 
 namespace fe {
 
