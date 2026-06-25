da7fa88308a08165889f043163cb4b86108e9a24
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-25 17:21:04 +0200

    only fi widnows yeah

--- src/Engine/Shader.hpp
+++ src/Engine/Shader.hpp
@@ -16,8 +16,8 @@ namespace fe {
 
 	std::string GetExecutableDirectory()
 	{
-		#ifdef _WIN32
-		#include <Windows.h>
+#ifdef _WIN32
+#include <Windows.h>
 		char exePath[MAX_PATH] = {0};
 		GetModuleFileNameA(NULL, exePath, MAX_PATH);
 		
@@ -27,6 +27,7 @@ namespace fe {
 		{
 			return fullPath.substr(0, lastSlash);
 		}
+#endif
 		return "";
 	}
 
