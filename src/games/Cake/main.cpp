23c99f535a5584b354e574d61c6b8b3e1ccf9848
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-25 03:38:22 +0200

    not much but somethign

--- src/games/Cake/main.cpp
+++ src/games/Cake/main.cpp
@@ -3,7 +3,7 @@
 int main() {
 
   std::cout << "Hiii" << std::endl;
-  std::cout << "You're super amazing!! I hope you like the show :3";
+  std::cout << "You're super amazing!! I hope you like the show :3" << std::endl;
 
   Cake game;
   game.Run();
