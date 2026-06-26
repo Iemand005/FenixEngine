cd85ab27e6a85d4b97d4691db3f5f471b4c99df5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-26 16:19:58 +0200

    wo sit deforming but no time

--- src/games/Tunnel/resources/shaders/VertexShader.glsl
+++ src/games/Tunnel/resources/shaders/VertexShader.glsl
@@ -10,13 +10,21 @@ out vec2 TexCoord;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-
+uniform float time;
+uniform float wobbleAmount;
 
 void main()
 {
-    vec4 worldPos = model * vec4(aPos, 1.0);
-    gl_Position = projection * view * worldPos;
+    vec3 pos = aPos;
 
+    float wobbleX = sin(time * 2.0 + pos.z * 0.5) * wobbleAmount;
+    float wobbleY = cos(time * 1.5 + pos.z * 0.3) * wobbleAmount;
+    float wobbleZ = sin(time * 1.8 + pos.z * 0.7) * wobbleAmount;
+
+    pos += vec3(wobbleX, wobbleY, wobbleZ);
+
+    vec4 worldPos = model * vec4(pos, 1.0);
+    gl_Position = projection * view * worldPos;
     mat3 normalMat = transpose(inverse(mat3(model)));
     Normal = normalMat * aNormal;
     FragPos = worldPos.xyz;
