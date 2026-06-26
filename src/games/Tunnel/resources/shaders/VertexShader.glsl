3db3c8b5e04cedcd61d1a125d1d1c19b9f65934c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-26 15:16:59 +0200

    wobel shaderre

--- src/games/Tunnel/resources/shaders/VertexShader.glsl
+++ src/games/Tunnel/resources/shaders/VertexShader.glsl
@@ -1,24 +1,33 @@
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoord;
-  
-out vec3 Normal;
-out vec3 FragPos;
-out vec2 TexCoord;
+#version 450
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoord;
 
+layout(binding = 0) uniform UniformBuffer {
+    mat4 projection;
+    mat4 view;
+    mat4 model;
+    float time;
+    float wobbleAmount;
+} ubo;
 
-void main()
-{
-    vec4 worldPos = model * vec4(aPos, 1.0);
-    gl_Position = projection * view * worldPos;
+layout(location = 0) out vec3 fragNormal;
+layout(location = 1) out vec2 fragTexCoord;
 
-    mat3 normalMat = transpose(inverse(mat3(model)));
-    Normal = normalMat * aNormal;
-    FragPos = worldPos.xyz;
-    TexCoord = aTexCoord;
+void main() {
+    vec3 pos = position;
+
+    float wobbleX = sin(ubo.time * 2.0 + pos.y * 0.5) * ubo.wobbleAmount;
+    float wobbleY = cos(ubo.time * 1.5 + pos.y * 0.3) * ubo.wobbleAmount;
+    float wobbleZ = sin(ubo.time * 1.8 + pos.y * 0.7) * ubo.wobbleAmount;
+
+    pos += vec3(wobbleX, wobbleY, wobbleZ);
+
+    float radiusWobble = 1.0 + sin(ubo.time * 2.5 + pos.y * 0.4) * 0.2;
+    pos = normalize(pos) * radiusWobble;
+
+    gl_Position = ubo.projection * ubo.view * ubo.model * vec4(pos, 1.0);
+    fragNormal = normal;
+    fragTexCoord = texCoord;
 }
