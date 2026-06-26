b7f793324263874b7d41a3cc8c32494ec1e45f5b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-26 16:19:34 +0200

    efsfr

--- src/games/Tunnel/resources/shaders/VertexShader.glsl
+++ src/games/Tunnel/resources/shaders/VertexShader.glsl
@@ -1,28 +1,19 @@
 #version 330 core
-
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
-
-uniform mat4 projection;
-uniform mat4 view;
-uniform mat4 model;
-uniform float time;
-uniform float wobbleAmount;
 
 out vec3 Normal;
 out vec3 FragPos;
 out vec2 TexCoord;
 
-void main() {
-    vec3 pos = position;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
-    float wobbleX = sin(time * 2.0 + pos.y * 0.5) * wobbleAmount;
-    float wobbleY = cos(time * 1.5 + pos.y * 0.3) * wobbleAmount;
-    float wobbleZ = sin(time * 1.8 + pos.y * 0.7) * wobbleAmount;
 
-    pos += vec3(wobbleX, wobbleY, wobbleZ);
-
+void main()
+{
     vec4 worldPos = model * vec4(aPos, 1.0);
     gl_Position = projection * view * worldPos;
 
