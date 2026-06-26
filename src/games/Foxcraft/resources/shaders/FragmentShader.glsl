3cda37213e8bc2061f1e17ed2f156f476e449d90
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-26 23:18:00 +0200

    why does changign shader break shti

--- src/games/Foxcraft/resources/shaders/FragmentShader.glsl
+++ src/games/Foxcraft/resources/shaders/FragmentShader.glsl
@@ -22,19 +22,7 @@ void main()
     vec3 n = normalize(Normal);
 
     vec3 lighting = vec3(0.1);
-    for (int i = 0; i < lightCount; ++i) {
-        vec3 L = pointLights[i].position - FragPos;
-        float dist = length(L);
-        if (dist < 0.0001) continue;
-        vec3 ldir = L / dist;
 
-        float diff = max(dot(n, ldir), 0.0);
-        float radius = max(pointLights[i].radius, 0.001);
-        float atten = 1.0 / (1.0 + (dist * dist) / (radius * radius));
-
-        vec3 contrib = pointLights[i].color * pointLights[i].intensity * diff * atten;
-        lighting += contrib;
-    }
 
     vec4 texSample = texture(ourTexture, TexCoord);
     FragColor = vec4(texSample.rgb * lighting, texSample.a);
