f527319b41bb8a24dc2c6c19b9b40c0862223f0b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-27 18:42:05 +0200

    waht the heeck how did I mess that up mybad

--- src/Engine/XRGame.cpp
+++ src/Engine/XRGame.cpp
@@ -1,6 +1,11 @@
 
 #include "XRGame.hpp"
 
+#define XR_USE_PLATFORM_XLIB
+
+#include <GL/glx.h>
+
+
 #include <openxr/openxr.h>
 #include <openxr/openxr_platform.h>
 
@@ -299,7 +304,7 @@ void XRGame::initOpenXR() {
       } else if (SDL_strcmp(video_driver, "x11") == 0) {
 
 
-        Display *xDisplay = window->GetX11Display();
+        Display *xDisplay = (Display *)window->GetX11Display();
 
         // --- 3. RETRIEVE THE GLX CONTEXT ---
         // Make sure the SDL context is current, then use standard GLX functions to peel back the wrapper
@@ -408,8 +413,7 @@ void XRGame::initOpenXR(void *next) {
 
 	impl->BeginSession();
 
-    XrReferenceSpaceCreateInfo	impl->outputError(xrCreateReferenceSpace(impl->session, &spaceInfo, &impl->appSpace));
- spaceInfo{XR_TYPE_REFERENCE_SPACE_CREATE_INFO};
+    XrReferenceSpaceCreateInfo spaceInfo{XR_TYPE_REFERENCE_SPACE_CREATE_INFO};
 	spaceInfo.referenceSpaceType = XR_REFERENCE_SPACE_TYPE_LOCAL;
 	spaceInfo.poseInReferenceSpace.position = {0, 0, 0};
 	spaceInfo.poseInReferenceSpace.orientation = {0, 0, 0, 1};
