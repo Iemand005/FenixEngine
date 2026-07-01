74c22ebf34557690273eecd056b4ffa7135a3d9f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-01 21:27:19 +0200

    reorder and ndety me

--- src/Engine/accessories/Aura.hpp
+++ src/Engine/accessories/Aura.hpp
@@ -4,16 +4,14 @@
 
 class Aura {
 
-    struct Impl;
-    std::unique_ptr<Impl> impl;
-
 public:
+	Aura();
+	~Aura();
 
-    Aura();
+	bool IsOpen() const;
+	bool SetColor(char r, char g, char b);
 
-    ~Aura();
-
-    bool IsOpen() const;
-
-    bool SetColor(char r, char g, char b);
+private:
+	struct Impl;
+	std::unique_ptr<Impl> impl;
 };
