0a3a286a063a2208cf26a40d2d678cf954588503
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-01 05:18:54 +0200

    destruction and proper init

--- src/Engine/accessories/aura.h
+++ src/Engine/accessories/aura.h
@@ -36,7 +36,7 @@ static_assert(sizeof(AuraColorReport) == 64, "AuraColorReport must be exactly 64
 
 class Aura {
 
-    HANDLE dev = nullptr;
+    HANDLE dev = NULL;
 
     HANDLE OpenAura(USHORT vid, USHORT pid, USHORT page, USHORT usage)
     {
@@ -75,7 +75,16 @@ class Aura {
     }
 
 public:
-    void SetColor(HANDLE dev, BYTE r, BYTE g, BYTE b)
+
+    Aura() {
+        this->dev = OpenAura(0x0B05, 0x19B6, 0xFF31, 0x76);;
+    }
+
+    ~Aura() {
+        CloseHandle(this->dev);
+    }
+
+    void SetColor(BYTE r, BYTE g, BYTE b)
     {
         AuraInitReport init;
         HidD_SetFeature(dev, &init, sizeof(init));
