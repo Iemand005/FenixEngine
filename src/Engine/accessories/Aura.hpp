0bf2c618eeee157e9e7c4f3a0d2c4b16f32678cb
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-07 18:43:46 +0200

    ok

--- src/Engine/accessories/Aura.hpp
+++ src/Engine/accessories/Aura.hpp
@@ -14,9 +14,7 @@ public:
 	BYTE lastR = 0, lastG = 0, lastB = 0;
 	bool auraInitialized = false;
 
-	void SetColorRGB(float r, float g, float b)
-	{
-
+	void SetColorFloat(float r, float g, float b) {
 		BYTE ir = (BYTE)(std::clamp(r, 0.0f, 1.0f) * 255.0f + 0.5f);
 		BYTE ig = (BYTE)(std::clamp(g, 0.0f, 1.0f) * 255.0f + 0.5f);
 		BYTE ib = (BYTE)(std::clamp(b, 0.0f, 1.0f) * 255.0f + 0.5f);
@@ -24,7 +22,7 @@ public:
 		if (auraInitialized && ir == lastR && ig == lastG && ib == lastB)
 			return;
 
-		aura.SetColor(ir, ig, ib);
+		SetColor(ir, ig, ib);
 		lastR = ir; lastG = ig; lastB = ib;
 		auraInitialized = true;
 	}
