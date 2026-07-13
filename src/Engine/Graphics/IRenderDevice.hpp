004cd0d593eb41a2e9f2602fcad939f725c42616
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 15:50:17 +0200

    interfaced clearcolour func

--- src/Engine/Graphics/IRenderDevice.hpp
+++ src/Engine/Graphics/IRenderDevice.hpp
@@ -13,7 +13,7 @@ namespace fe {
 		virtual ~IRenderDevice() = default;
 		virtual void Init(fe::IWindow *window) = 0;
 		virtual void Clear() = 0;
-		virtual void SetClearColor(float r, float g, float b, float a = 1);
+		virtual void SetClearColor(float r, float g, float b, float a = 1) = 0;
 		// virtual VertexBuffer* CreateVertexBuffer(void* data, size_t size) = 0;
 		// virtual Texture* CreateTexture(const std::string& path) = 0;
 		// virtual void DrawIndexed(uint32_t indexCount) = 0;
