796ac53ea7b6b5f306a4173da62b0ccdc225650b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 15:53:01 +0200

    reduce reusse

--- src/Engine/Game.cpp
+++ src/Engine/Game.cpp
@@ -1,20 +1,4 @@
-#include <glad/glad.h>
-
 #include "Game.hpp"
 
 using namespace fe;
 
-void fe::Game::ToggleWireframe(bool enabled) {
-	if (enabled) EnableWireframe();
-	else DisableWireframe();
-}
-
-void Game::BindFrameBuffer(int bufferIndex) {
-	glBindFramebuffer(GL_FRAMEBUFFER, bufferIndex);
-}
-
-void Game::CheckErrors() {
-	GLenum err;
-	while ((err = glGetError()) != GL_NO_ERROR)
-		std::cerr << "OpenGL error: " << err << std::endl;
-}
