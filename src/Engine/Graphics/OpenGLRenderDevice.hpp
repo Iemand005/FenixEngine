3b82e10f2d4b6fc10cb53f0d7279767e231562c4
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 12:58:38 +0200

    fix messing with your hips

--- src/Engine/Graphics/OpenGLRenderDevice.hpp
+++ src/Engine/Graphics/OpenGLRenderDevice.hpp
@@ -13,32 +13,29 @@ void uploadMesh(Mesh<VertexType>& mesh) {
 
         glBindVertexArray(glBuffers->vao);
 
-        // Vertex Buffer vullen
         glBindBuffer(GL_ARRAY_BUFFER, glBuffers->vbo);
         glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(VertexType), mesh.vertices.data(), GL_STATIC_DRAW);
 
-        // Index Buffer vullen
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glBuffers->ebo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.indices.size() * sizeof(uint32_t), mesh.indices.data(), GL_STATIC_DRAW);
 
-        // DYNAMISCHE ATTRIBUTEN CONFIGURATIE
         int vertexStride = sizeof(VertexType);
-        auto layout = VertexType::getLayout(); // Haalt de layout op van de specifieke struct
+        auto layout = VertexType::getLayout();
 
         for (const auto& attr : layout) {
             glVertexAttribPointer(
                 attr.location, 
-                attr.components, // Dit wordt nu dynamisch 2 of 3!
+                attr.components,
                 GL_FLOAT, 
                 GL_FALSE, 
                 vertexStride, 
-                (void*)attr.offset // De exacte byte-offset via offsetof
+                (void*)attr.offset
             );
             glEnableVertexAttribArray(attr.location);
         }
 
         glBindVertexArray(0); // Unbind
 
-        // Geef buffers aan de mesh
         mesh.gpuBuffers = std::move(glBuffers);
     }
+};
