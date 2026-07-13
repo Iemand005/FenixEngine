d29bc05116a73aa0a8cc7aa0ea43ea92ba5ed026
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 15:52:38 +0200

    functions shoved down to renderer level

--- src/Engine/Game.cpp
+++ src/Engine/Game.cpp
@@ -4,21 +4,6 @@
 
 using namespace fe;
 
-void Game::InitGL() {
-	glEnable(GL_MULTISAMPLE);
-}
-
-void Game::SetClearColor(float r, float g, float b, float a) {
-	glClearColor(r, g, b, a);
-}
-
-void Game::EnableWireframe() {
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-}
-void Game::DisableWireframe() {
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-}
-
 void fe::Game::ToggleWireframe(bool enabled) {
 	if (enabled) EnableWireframe();
 	else DisableWireframe();
