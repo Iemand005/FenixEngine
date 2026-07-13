b4faaea9c649659464199d5ce5b3015388bf2fa4
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 13:27:14 +0200

    be damned

--- src/Engine/Graphics/OpenGLGPUBuffers.hpp
+++ src/Engine/Graphics/OpenGLGPUBuffers.hpp
@@ -6,15 +6,12 @@
 #include "IGPUBuffers.hpp"
 
 class OpenGLGPUBuffers : public IGPUBuffers {
-	unsigned int vao = 0;
-	unsigned int VBO = 0;
-	unsigned int EBO = 0;
-	unsigned int texture = 0;
+	unsigned int vao = 0, vbo = 0, ebo = 0, texture = 0;
 
 public:
 
 	~OpenGLGPUBuffers() override {
-        if (EBO != 0) glDeleteBuffers(1, &ebo);
+        if (ebo != 0) glDeleteBuffers(1, &ebo);
         if (vbo != 0) glDeleteBuffers(1, &vbo);
         if (vao != 0) glDeleteVertexArrays(1, &vao);
     }
