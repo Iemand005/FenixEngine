d876a21f5f5232d4956820f8de0c3525512ea6bc
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 17:18:16 +0200

    gl loadproc not a chance in the world

--- src/Engine/Graphics/Renderer.cpp
+++ src/Engine/Graphics/Renderer.cpp
@@ -17,9 +17,11 @@ void Renderer::ToggleWireframe(bool enabled) {
 }
 
 Renderer::Renderer(GLADloadproc loadProc) {
+  renderDevice = std::make_unique<OpenGLRenderDevice>();
   if (!gladLoadGLLoader(loadProc)) {
     std::cerr << "Failed to load OpenGL functions (GLAD)";
   }
+  renderDevice->Init(nullptr);
   this->SetClearColor(0.1f, 0.4f, 1.0f);
 }
 
