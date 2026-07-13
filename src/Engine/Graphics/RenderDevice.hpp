06412fa41089f51c029d05a8fb8dbd74be83fd26
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 04:36:13 +0200

    window passage with interface

--- src/Engine/Graphics/RenderDevice.hpp
+++ src/Engine/Graphics/RenderDevice.hpp
@@ -4,7 +4,7 @@
 class RenderDevice {
 public:
     virtual ~RenderDevice() = default;
-	virtual void Init() = 0;
+	virtual void Init(fe::IWindow *window) = 0;
     // virtual VertexBuffer* CreateVertexBuffer(void* data, size_t size) = 0;
     // virtual Texture* CreateTexture(const std::string& path) = 0;
     // virtual void DrawIndexed(uint32_t indexCount) = 0;
