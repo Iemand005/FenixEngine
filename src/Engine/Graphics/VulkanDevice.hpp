90e165d86a03b2df5b18449599a7db66f3c12e4d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 04:55:37 +0200

    call vk surface creation, but now I'm gonna expect to see conflicts becaus ew already hav opgnel surface now

--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -31,6 +31,7 @@ class VulkanDevice : public RenderDevice {
 public:
 	void Init(fe::IWindow *window) override {
 		createInstance(window);
+		createSurface(window);
 	}
     // VertexBuffer* CreateVertexBuffer(void* data, size_t size) override {
     //     return new VulkanVertexBuffer(data, size); // Uses vkCreateBuffer, vkBindBufferMemory
