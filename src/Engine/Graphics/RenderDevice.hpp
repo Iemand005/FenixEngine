f0a5bf6dfea1d57dff1d98c469431a5e4105dc8c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 04:00:56 +0200

    inlcude vulkan headers uh

--- src/Engine/Graphics/RenderDevice.hpp
+++ src/Engine/Graphics/RenderDevice.hpp
@@ -4,6 +4,6 @@ public:
 	virtual void Init() = 0;
     // virtual VertexBuffer* CreateVertexBuffer(void* data, size_t size) = 0;
     // virtual Texture* CreateTexture(const std::string& path) = 0;
-    virtual void DrawIndexed(uint32_t indexCount) = 0;
+    // virtual void DrawIndexed(uint32_t indexCount) = 0;
     virtual void SubmitFrame() = 0;
 };
--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include "RenderDevice.hpp"
 
+#include <vulkan/vulkan.h>
+
 class VulkanDevice : public RenderDevice {
 public:
+	void Init() override {
+
+	}
     // VertexBuffer* CreateVertexBuffer(void* data, size_t size) override {
     //     return new VulkanVertexBuffer(data, size); // Uses vkCreateBuffer, vkBindBufferMemory
     // }
