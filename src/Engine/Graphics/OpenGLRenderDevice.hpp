07e81e1a60fcafe8b797fc8734e45001a8b0688d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 13:47:00 +0200

    override funcs to not make abstrct

--- src/Engine/Graphics/OpenGLRenderDevice.hpp
+++ src/Engine/Graphics/OpenGLRenderDevice.hpp
@@ -4,46 +4,55 @@
 #include "IRenderDevice.hpp"
 
 class OpenGLRenderDevice : public IRenderDevice {
-	void uploadMesh(Mesh<VertexType>& mesh) {
-        auto glBuffers = std::make_unique<OpenGLGPUBuffers>();
+	// void uploadMesh(Mesh<VertexType>& mesh) {
+    //     auto glBuffers = std::make_unique<OpenGLGPUBuffers>();
 
-        glGenVertexArrays(1, &glBuffers->vao);
-        glGenBuffers(1, &glBuffers->vbo);
-        glGenBuffers(1, &glBuffers->ebo);
+    //     glGenVertexArrays(1, &glBuffers->vao);
+    //     glGenBuffers(1, &glBuffers->vbo);
+    //     glGenBuffers(1, &glBuffers->ebo);
 
-        glBindVertexArray(glBuffers->vao);
+    //     glBindVertexArray(glBuffers->vao);
 
-        glBindBuffer(GL_ARRAY_BUFFER, glBuffers->vbo);
-        glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(VertexType), mesh.vertices.data(), GL_STATIC_DRAW);
+    //     glBindBuffer(GL_ARRAY_BUFFER, glBuffers->vbo);
+    //     glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(VertexType), mesh.vertices.data(), GL_STATIC_DRAW);
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glBuffers->ebo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.indices.size() * sizeof(uint32_t), mesh.indices.data(), GL_STATIC_DRAW);
+    //     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glBuffers->ebo);
+    //     glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.indices.size() * sizeof(uint32_t), mesh.indices.data(), GL_STATIC_DRAW);
 
-        int vertexStride = sizeof(VertexType);
-        auto layout = VertexType::getLayout();
+    //     int vertexStride = sizeof(VertexType);
+    //     auto layout = VertexType::getLayout();
 
-        for (const auto& attr : layout) {
-            glVertexAttribPointer(
-                attr.location, 
-                attr.components,
-                GL_FLOAT, 
-                GL_FALSE, 
-                vertexStride, 
-                (void*)attr.offset
-            );
-            glEnableVertexAttribArray(attr.location);
-        }
+    //     for (const auto& attr : layout) {
+    //         glVertexAttribPointer(
+    //             attr.location, 
+    //             attr.components,
+    //             GL_FLOAT, 
+    //             GL_FALSE, 
+    //             vertexStride, 
+    //             (void*)attr.offset
+    //         );
+    //         glEnableVertexAttribArray(attr.location);
+    //     }
 
-        glBindVertexArray(0);
+    //     glBindVertexArray(0);
 
-        mesh.gpuBuffers = std::move(glBuffers);
+    //     mesh.gpuBuffers = std::move(glBuffers);
+    // }
+
+    void Init(fe::IWindow *window) {
+
     }
 
 
-    void DrawMesh(const fe::Mesh& mesh) override {
+
+    void DrawMesh(const fe::Mesh<>& mesh) override {
         if (mesh.gpuBuffers) {
             mesh.gpuBuffers->bind();
             glDrawElements(GL_TRIANGLES, mesh.indices.size(), GL_UNSIGNED_INT, 0);
         }
+    }
+
+    void SubmitFrame() {
+        
     }
 };
