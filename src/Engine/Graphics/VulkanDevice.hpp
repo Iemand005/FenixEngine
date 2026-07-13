c5767c39c7139a37127fbba692b7998f6338813a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 05:43:39 +0200

    s add deps n shit

--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -54,6 +54,83 @@ struct SwapChainSupportDetails {
 };
 
 
+struct Vertex {
+    glm::vec3 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription binding{};
+        binding.binding = 0;
+        binding.stride = sizeof(Vertex);
+        binding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return binding;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attrs{};
+        attrs[0].binding = 0;
+        attrs[0].location = 0;
+        attrs[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attrs[0].offset = offsetof(Vertex, pos);
+
+        attrs[1].binding = 0;
+        attrs[1].location = 1;
+        attrs[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attrs[1].offset = offsetof(Vertex, color);
+
+        return attrs;
+    }
+};
+
+struct UniformBufferObject {
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 proj;
+};
+
+const std::vector<Vertex> kCubeVertices = {
+    // Front face (red)
+    {{-0.5f, -0.5f,  0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{ 0.5f, -0.5f,  0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{ 0.5f,  0.5f,  0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{-0.5f,  0.5f,  0.5f}, {1.0f, 0.0f, 0.0f}},
+    // Back face (green)
+    {{ 0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{-0.5f,  0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{ 0.5f,  0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    // Left face (blue)
+    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{-0.5f, -0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{-0.5f,  0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{-0.5f,  0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}},
+    // Right face (yellow)
+    {{ 0.5f, -0.5f,  0.5f}, {1.0f, 1.0f, 0.0f}},
+    {{ 0.5f, -0.5f, -0.5f}, {1.0f, 1.0f, 0.0f}},
+    {{ 0.5f,  0.5f, -0.5f}, {1.0f, 1.0f, 0.0f}},
+    {{ 0.5f,  0.5f,  0.5f}, {1.0f, 1.0f, 0.0f}},
+    // Top face (cyan)
+    {{-0.5f,  0.5f,  0.5f}, {0.0f, 1.0f, 1.0f}},
+    {{ 0.5f,  0.5f,  0.5f}, {0.0f, 1.0f, 1.0f}},
+    {{ 0.5f,  0.5f, -0.5f}, {0.0f, 1.0f, 1.0f}},
+    {{-0.5f,  0.5f, -0.5f}, {0.0f, 1.0f, 1.0f}},
+    // Bottom face (magenta)
+    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 1.0f}},
+    {{ 0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 1.0f}},
+    {{ 0.5f, -0.5f,  0.5f}, {1.0f, 0.0f, 1.0f}},
+    {{-0.5f, -0.5f,  0.5f}, {1.0f, 0.0f, 1.0f}},
+};
+
+const std::vector<uint32_t> kCubeIndices = {
+     0,  1,  2,   2,  3,  0,  // front
+     4,  5,  6,   6,  7,  4,  // back
+     8,  9, 10,  10, 11,  8,  // left
+    12, 13, 14,  14, 15, 12,  // right
+    16, 17, 18,  18, 19, 16,  // top
+    20, 21, 22,  22, 23, 20,  // bottom
+};
+
+
 class VulkanDevice : public RenderDevice {
 public:
 	void Init(fe::IWindow *window) override {
@@ -1224,8 +1301,5 @@ private:
         if (device_ != VK_NULL_HANDLE) vkDestroyDevice(device_, nullptr);
         if (surface_ != VK_NULL_HANDLE) vkDestroySurfaceKHR(instance_, surface_, nullptr);
         if (instance_ != VK_NULL_HANDLE) vkDestroyInstance(instance_, nullptr);
-
-        if (window_) glfwDestroyWindow(window_);
-        glfwTerminate();
     }
 };
