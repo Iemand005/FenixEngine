2dc1d6e432552e83ac5f27a7d49ee2d27c738962
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 04:03:30 +0200

    override each becaus eit sasy sit'v irtual??

--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -11,4 +11,7 @@ public:
     // VertexBuffer* CreateVertexBuffer(void* data, size_t size) override {
     //     return new VulkanVertexBuffer(data, size); // Uses vkCreateBuffer, vkBindBufferMemory
     // }
+
+	~RenderDevice() override {}
+	void SubmitFrame() override {}	
 };
