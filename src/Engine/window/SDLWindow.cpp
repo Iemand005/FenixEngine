2968f220aa00409a611423ba70f22356fa94be5a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-14 22:21:43 +0200

    gl debug stuff

--- src/Engine/window/SDLWindow.cpp
+++ src/Engine/window/SDLWindow.cpp
@@ -92,6 +92,7 @@ fe::SDLWindow::SDLWindow(std::string title, int width, int height, bool hidden, 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 
 	bool tenBit = true;
 
