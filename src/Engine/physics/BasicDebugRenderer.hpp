889f0c00e23cd05a45365b4f691124e9aefcfff5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-14 16:53:49 +0200

    yoo clangd shows chi imports I'm not using!

--- src/Engine/physics/BasicDebugRenderer.hpp
+++ src/Engine/physics/BasicDebugRenderer.hpp
@@ -2,13 +2,11 @@
 
 #include <vector>
 #include <string>
-#include <memory>
 #include <map>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-// #include <imgui/imgui.h>
 
 #include <Jolt/Jolt.h>
 #include <Jolt/Renderer/DebugRendererSimple.h>
