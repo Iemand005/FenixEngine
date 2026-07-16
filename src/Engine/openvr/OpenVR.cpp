eedff8bafb56c16c1ff7d6dd863913ebc27e5344
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-16 18:01:19 +0200

    brack

--- src/Engine/openvr/OpenVR.cpp
+++ src/Engine/openvr/OpenVR.cpp
@@ -8,4 +8,11 @@ using namespace fe;
 
 OpenVR::Init() {
 	std::cout << "Initializing OpenVR..." << std::endl;
+
+	vr::EVRInitError initError = vr::VRInitError_None;
+    vr::IVRSystem* vrSystem = vr::VR_Init(&initError, vr::VRApplication_Scene);
+}
+
+OpenVR::Shutdown() {
+	vr::VR_Shutdown();
 }
