c399ec10954304ed671758caaaa223797d1eede1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-16 22:06:06 +0200

    badadadadamndan

--- src/Engine/openvr/OpenVR.hpp
+++ src/Engine/openvr/OpenVR.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <glad/glad.h>
+
+namespace vr {
+	struct VROverlayHandle_t_;
+	typedef uint64_t VROverlayHandle_t;
+}
+
 namespace fe {
 	class OpenVR {
-public:
+	public:
+		vr::VROverlayHandle_t overlayHandle;
+		GLuint overlayFbo = 0;
+		GLuint overlayTexture = 0;
+		GLuint overlayDepth = 0;
+		int fboWidth = 0;
+		int fboHeight = 0;
+
 		void Init();
 		void InitOverlay();
+		void CreateOverlayTexture(int width, int height);
+		void CaptureAndSubmit();
 		void Shutdown();
 	};
 }
