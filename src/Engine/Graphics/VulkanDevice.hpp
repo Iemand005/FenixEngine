317c8431e11d995df394ad72365b4c863fd6677a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-17 17:36:03 +0200

    thorw it at a g

--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -126,6 +126,7 @@ public:
 		createDescriptorPool();
 		createDefaultTexture();
 		createDescriptorSets();
+		createFrameDescriptorSets();
 		createCommandBuffers();
 		createSyncObjects();
 	}
@@ -1557,7 +1558,7 @@ private:
 	void createDescriptorPool() {
 		std::array<VkDescriptorPoolSize, 2> poolSizes{};
 		poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-		poolSizes[0].descriptorCount = static_cast<uint32_t>(kMaxDrawsPerFrame * kMaxFramesInFlight);
+		poolSizes[0].descriptorCount = static_cast<uint32_t>(kMaxDrawsPerFrame * kMaxFramesInFlight + kMaxFramesInFlight);
 		poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 		poolSizes[1].descriptorCount = static_cast<uint32_t>(kMaxDrawsPerFrame * kMaxFramesInFlight);
 
@@ -1566,7 +1567,7 @@ private:
 		poolInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
 		poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
 		poolInfo.pPoolSizes = poolSizes.data();
-		poolInfo.maxSets = static_cast<uint32_t>(kMaxDrawsPerFrame * kMaxFramesInFlight);
+		poolInfo.maxSets = static_cast<uint32_t>(kMaxDrawsPerFrame * kMaxFramesInFlight + kMaxFramesInFlight);
 
 		if (vkCreateDescriptorPool(device_, &poolInfo, nullptr, &descriptorPool_) != VK_SUCCESS) {
 			throw std::runtime_error("Failed to create descriptor pool.");
