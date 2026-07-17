2670dbec35d9d6a424c4175a379cf7c0edbff3a5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-17 21:53:42 +0200

    tenbit stuff

--- src/Engine/window/SDLWindow.cpp
+++ src/Engine/window/SDLWindow.cpp
@@ -118,10 +118,8 @@ fe::SDLWindow::SDLWindow(std::string title, int width, int height, bool hidden, 
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 
-		bool tenBit = true;
-
+		bool tenBit = options.tenBit;
 		if (tenBit) {
 			SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 10);
 			SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 10);
