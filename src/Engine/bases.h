414ea4bf3167a45ab1906d041b8d40ddb3cec624
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-18 19:36:42 +0200

    tabss!

--- src/Engine/bases.h
+++ src/Engine/bases.h
@@ -2,19 +2,19 @@
 #include <glm/glm.hpp>
 
 namespace fe {
-  struct ObjectState {
-    glm::vec3 position{0.0f};
-    glm::vec3 rotation{0.0f};
-    glm::vec3 velocity{0.0f};
-    glm::vec3 scale{1.0f};
-  };
+	struct ObjectState {
+		glm::vec3 position{0.0f};
+		glm::vec3 rotation{0.0f};
+		glm::vec3 velocity{0.0f};
+		glm::vec3 scale{1.0f};
+	};
 
-  enum Direction {
-    Forwards,
-    Backwards,
-    Left,
-    Right,
-    Up,
-    Down
-  };
+	enum Direction {
+		Forwards,
+		Backwards,
+		Left,
+		Right,
+		Up,
+		Down
+	};
 }
