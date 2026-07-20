c398ce69e226812671b71c7385e3b0d2a3f06d4d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-20 22:26:48 +0200

    move vkdev impl

--- src/Engine/Graphics/VulkanDevice.hpp
+++ src/Engine/Graphics/VulkanDevice.hpp
@@ -1065,40 +1065,7 @@ private:
 
 
 
-	void pickPhysicalDevice() {
-		uint32_t deviceCount = 0;
-		vkEnumeratePhysicalDevices(_instance, &deviceCount, nullptr);
-		if (deviceCount == 0)
-			throw std::runtime_error("No GPUs with Vulkan support found.");
-
-		std::vector<VkPhysicalDevice> devices(deviceCount);
-		vkEnumeratePhysicalDevices(_instance, &deviceCount, devices.data());
-
-		int bestScore = -1;
-		VkPhysicalDevice bestDevice = VK_NULL_HANDLE;
-
-		for (const auto& dev : devices) {
-			int score = rateDeviceSuitability(dev);
-			VkPhysicalDeviceProperties props;
-			vkGetPhysicalDeviceProperties(dev, &props);
-			std::cout << "Found GPU: " << props.deviceName << " (score " << score << ")\n";
-
-			if (score > bestScore) {
-				bestScore = score;
-				bestDevice = dev;
-			}
-		}
-
-		if (bestDevice == VK_NULL_HANDLE || bestScore < 0) {
-			throw std::runtime_error("No suitable GPU found.");
-		}
-
-		physicalDevice_ = bestDevice;
-		VkPhysicalDeviceProperties props;
-		vkGetPhysicalDeviceProperties(physicalDevice_, &props);
-		deviceName_ = props.deviceName;
-		std::cout << "Selected GPU: " << deviceName_ << "\n";
-	}
+	void pickPhysicalDevice();
 
 	void createLogicalDevice() {
 		QueueFamilyIndices indices = findQueueFamilies(physicalDevice_);
