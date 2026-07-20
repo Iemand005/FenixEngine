19fd45a9ccfa3f7beccfa8c0526650b2fddd4d92
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-20 18:05:50 +0200

    engine defs hmm

--- src/Engine/Sensors/Accelerometer.hpp
+++ src/Engine/Sensors/Accelerometer.hpp
@@ -1,12 +1,16 @@
 
 #ifdef WIN32
+#define WINRT_LEAN_AND_MEAN
+#define _AMD64_
+#include <winrt/impl/Windows.Devices.Sensors.2.h>
 #include <winrt/Windows.Devices.Sensors.h>
 
-using namespace winrt::Windows::Devices::Sensors;
+using namespace winrt;
 #endif
 
 namespace fe {
 	class Accelerometer {
+		Windows::Devices::Sensors::Accelerometer accelerometer;
 
 	};
 };
