87a7e680df9b1e0cb6b18c109f675abecd31f9e5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-21 19:46:11 +0200

    include cglftsf

--- src/Engine/ModelLoader.cpp
+++ src/Engine/ModelLoader.cpp
@@ -1,8 +1,16 @@
 #include "ModelLoader.hpp"
 
+#include <cgltf.h>
+
 using namespace fe;
 
 
-// Object<> ModelLoader::LoadModel(std::string fileName) {
+Object ModelLoader::LoadMesh(std::string fileName) {
+	Object result;
 
-// }
+	cgltf_options options = {};
+	cgltf_data* data = nullptr;
+	cgltf_result parse_result = cgltf_parse_file(&options, fileName.c_str(), &data);
+
+	return std::move(result);
+}
