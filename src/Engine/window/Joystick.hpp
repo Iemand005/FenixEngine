49de897a98a6cf83183af046fc8dbe83d80985ea
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-21 15:34:42 +0200

    alone again

--- src/Engine/window/Joystick.hpp
+++ src/Engine/window/Joystick.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <stringx>
 
 namespace fe {
 	class Joystick {
 public:
 		Joystick(unsigned int id) {
 			joystick = id;
+		}
+
+		std:: string GetName() {
+			return SDL_GetJoystickNameForID(joystickIds[i]);
 		}
 private:
 	SDL_JoystickID joystick;
