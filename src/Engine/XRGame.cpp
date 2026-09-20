befc2008311b6abff79a0d9de6760526bd99d1d3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-21 01:27:04 +0200

    scene queening out toneight!

--- src/Engine/XRGame.cpp
+++ src/Engine/XRGame.cpp
@@ -710,7 +710,7 @@ void XRGame::DestroyXR() {
 void XRGame::LaunchVR() {
 	#ifndef FE_EXCLUDE_OPENXR
 	initOpenXR();
-	if (!IsInstanceValid()) {
+np	if (!IsInstanceValid()) {
 		fe::LogWarning("OpenXR instance invalid or no runtime found - staying in standard mode");
 		return;
 	}
