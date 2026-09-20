bf4eb04091bf2823e70c85c61f77be8998988773
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-21 01:49:47 +0200

    bruh ok

--- src/Engine/Graphics/OpenGLGPUTexture.cpp
+++ src/Engine/Graphics/OpenGLGPUTexture.cpp
@@ -1,7 +1,14 @@
 
 #include <vector>
 
+#if defined(__ANDROID__)
+#include <GLES3/gl31.h>
+#include <GLES3/gl3ext.h>
+#elif defined(__EMSCRIPTEN__)
+#include <GLES3/gl3.h>
+#else
 #include <glad/glad.h>
+#endif
 
 #include "OpenGLGPUTexture.hpp"
 #include "../ImageLoader.hpp"
--- src/Engine/Graphics/OpenGLGPUTexture.hpp
+++ src/Engine/Graphics/OpenGLGPUTexture.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#if defined(__ANDROID__)
+#include <GLES3/gl31.h>
+#include <GLES3/gl3ext.h>
+#elif defined(__EMSCRIPTEN__)
+#include <GLES3/gl3.h>
+#else
 #include <glad/glad.h>
+#endif
 #include "IGPUTexture.hpp"
 #include "../ImageLoader.hpp"
 #include <unordered_map>
